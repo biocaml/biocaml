7a0aef3656fa3d0455ae2aff081f4326df96cb38
Ashish Agarwal <agarwal1975@gmail.com>
2009-03-31 06:58:27 +0000

    ChrName made more efficient by pre-compiling pattern to regexp.
    git-svn-id: svn+ssh://svn.gersteinlab.org/ocaml/biocaml@39 aded56cc-775a-46a7-b2d2-b715e5f81702

--- src/bioCaml/chrName.ml
+++ src/bioCaml/chrName.ml
@@ -16,25 +16,26 @@ module Alpha = struct
   let all = [x;y;m;mt;mtdna]
 end
 
+let rex = Pcre.regexp "^(chr|CHR|Chr)?(.*)$"
 let of_string s =
   try
-    let ss = Pcre.extract ~pat:"^(chr|CHR|Chr)?(.*)$" s in
-    let _ = assert(Array.length ss = 3) in
-    let c = ss.(2) in
-      if c = Alpha.x then
-        ChrX
-      else if c = Alpha.y then
-        ChrY
-      else if c = Alpha.m || c = Alpha.mt || c = Alpha.mtdna then
-        ChrM
-      else
-        match RomanNum.of_string c with
-          | Some n -> ChrN (RomanNum.to_int n)
-          | None ->
-              try
-                let n = int_of_string c in
-                  if n > 0 then ChrN n else Unknown s
-              with Failure _ -> Unknown s
+    let ss = Pcre.extract ~full_match:false ~rex s in
+    assert(Array.length ss = 2);
+    let c = ss.(1) in
+    if c = Alpha.x then
+      ChrX
+    else if c = Alpha.y then
+      ChrY
+    else if c = Alpha.m || c = Alpha.mt || c = Alpha.mtdna then
+      ChrM
+    else
+      match RomanNum.of_string c with
+        | Some n -> ChrN (RomanNum.to_int n)
+        | None ->
+            try
+              let n = int_of_string c in
+              if n > 0 then ChrN n else Unknown s
+            with Failure _ -> Unknown s
   with
       Not_found -> Unknown s
         
@@ -49,16 +50,16 @@ let to_arabic t =
   match t with
     | ChrX | ChrY | ChrM | Unknown _ -> non_num_to_string t
     | ChrN n -> chr ^ (string_of_int n)
-      
+        
 let to_roman t =
   match t with
     | ChrX | ChrY | ChrM | Unknown _ -> non_num_to_string t
     | ChrN n ->
         let n = RomanNum.to_string (RomanNum.of_int_exn n) in
-          if List.mem n Alpha.all
-          then failwith (sprintf "chromosome %s cannot be represented in Roman form" (to_arabic t))
-          else chr ^ n
-            
+        if List.mem n Alpha.all
+        then failwith (sprintf "chromosome %s cannot be represented in Roman form" (to_arabic t))
+        else chr ^ n
+          
 let arabic = of_string ->> to_arabic
 let roman = of_string ->> to_roman
 
