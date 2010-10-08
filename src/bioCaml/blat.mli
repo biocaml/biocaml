cafc2fda4dd0594e9b93f4fe14722adf00dc92c3
skyr <skyr@skyr.(none)>
2010-10-08 18:44:31 +0300

    Added a commandline interface to BLAT with a helper module

---
+++ src/bioCaml/blat.mli
@@ -0,0 +1,26 @@
+type psl_record = {
+  psl_matches : int;
+  psl_misMatches : int;
+  psl_repMatches : int;
+  psl_nCount : int;
+  psl_qNumInsert : int;
+  psl_qBaseInsert : int;
+  psl_tNumInsert : int;
+  psl_tBaseInsert : int;
+  psl_strand : bool * bool;
+  psl_qName : string;
+  psl_qSize : int;
+  psl_qStart : int;
+  psl_qEnd : int;
+  psl_tName : string;
+  psl_tSize : int;
+  psl_tStart : int;
+  psl_tEnd : int;
+  psl_blockCount : int;
+  psl_blockSizes : int list;
+  psl_qStarts : int list;
+  psl_tStarts : int list;
+}
+
+val invoke_blat_files : string -> string -> string -> string -> string list
+val invoke_blat_mem : string -> BioCaml.Fasta.t -> BioCaml.Fasta.t -> psl_record list
---
+++ src/bioCaml/invokeCmdline.mli
@@ -0,0 +1,2 @@
+val invoke_cmd : string -> string list -> string list
+val parse_lines : string -> (string -> 'a option) -> 'a list
