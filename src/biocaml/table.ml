1d09396bf052bdcd73aabea8350b13b337357918
Ashish Agarwal <agarwal1975@gmail.com>
2010-12-20 13:40:06 -0500

    Complete rewrite of Table: using Batteries instead of Sesame, and using Tags to support variations in supported formats.

--- src/biocaml/table.ml
+++ src/biocaml/table.ml
@@ -1,8 +1,6 @@
-module List = List2
-module Array = Array2
-module String = String2
-open Pervasives2
-open Printf
+open Batteries_uni;; open Printf
+
+exception Invalid of string
 
 type column_names = int * (int StringMap.t)
     (* number of columns, and map from column name to column number,
@@ -12,110 +10,70 @@ type row = string array
 type getter = row -> string -> string
 type columns = string list
 
-let parse_column_names s =
-  let sl = String.nsplit s "\t" in
-  let f (column_num, ans) column_name =
-    if StringMap.mem column_name ans then
-      failwith (sprintf "duplicate column name: %s" column_name)
+let make_getter columns =
+  let f i elt accum =
+    if StringMap.mem elt accum then
+      raise (Invalid (sprintf "found duplicate column name %s" elt))
     else
-      column_num+1, StringMap.add column_name column_num ans
+      StringMap.add elt i accum
   in
-  List.fold_left f (0,StringMap.empty) sl
-      
-let parse_row s = Array.of_list (String.nsplit s "\t")
-  
-let get ((num_cols, cols) : column_names) r x =
-  try
-    let i = StringMap.find x cols in
-    let n = Array.length r in
-    if n > num_cols then
-      failwith (sprintf "expecting %d columns but this row has %d" num_cols n)
-    else if i >= n then
-      ""
+  let map = columns |> List.enum |> Enum.foldi f StringMap.empty in
+  fun row col ->
+    try row.(StringMap.find col map)
+    with Invalid_argument _ -> raise (Invalid (sprintf "row does not have data for column %s" col))
+
+let enum_input ?(itags="table,comment-char=#,header,header_,separator=\\t") cin =
+  let itags = Tags.of_string itags in
+  let e = IO.lines_of cin in
+  if not **> Tags.tag_is "table" "true" itags then raise (Tags.Invalid "table=true tag required");
+  let separator = Tags.find "separator" itags in
+
+  (* parse comments if needed and return enumeration starting after comments *)
+  let comments, e =
+    try
+      let comment_char = (Tags.find "comment-char" itags).[0] in
+      let e1,e2 = Enum.span (Comments.is_comments ~comment_char) e in
+      Some (e1 |> Enum.map (Comments.of_string ~comment_char)
+      |> List.of_enum |> Comments.concat),
+      e2
+    with Not_found -> None, e
+  in
+
+  (* parse header if needed or None otherwise *)
+  let columns_opt =
+    if Tags.tag_is "header" "true" itags then
+      let columns = match Enum.get e with
+        | None -> raise (Invalid "expected headers but reached end-of-input")
+        | Some s -> String.nsplit s separator
+      in
+      (if Tags.tag_is "header_" "true" itags then
+        match Enum.get e with
+          | None -> raise (Invalid "expected header underline but reached end-of-input")
+          | Some s ->
+              let pred c = Char.is_whitespace c || c = '-' in
+              if s |> String.enum |> Enum.for_all pred then
+                ()
+              else
+                raise (Invalid "header line must consist of only whitespace or \'-\'") )
+      ;
+      Some columns
     else
-      r.(i)
-  with
-      Not_found -> failwith (sprintf "invalid column name: %s" x)
+      None
+  in
 
-let make_iter file =
-  let columns cin = 
-    try parse_column_names (input_line cin)
-    with Failure msg -> failwith (Msg.err ~pos:(Pos.fl file 1) msg)
+  (* return previously parsed column names or generate by peeking ahead to first data row *)
+  let columns = match columns_opt, Enum.peek e with
+    | None, None -> []
+    | None, Some s ->
+        (s
+        |> flip String.nsplit separator
+        |> List.length |> (fun n -> 0 -- (n-1))
+        |> Enum.map ((^) "column" -| string_of_int)
+        |> List.of_enum)
+    | Some columns, _ -> columns
   in
-  let iter f cin = (* cin should point to beginning of file *)
-    ignore (input_line cin);
-    try Lines.iter_channel (f <<- parse_row) cin
-    with Lines.Error (pos,msg) ->
-      let pos = Pos.incrl pos 1 in (* because skipped first line *)
-      let pos = Pos.set_file pos file in
-      failwith (Msg.err ~pos msg)
-  in
-  let columns file = try_finally columns close_in (open_in file) in
-  let iter f file = try_finally (iter f) close_in (open_in file) in
-  let (_,col_map) as cols = columns file in
-  let col_list = (StringMap.to_list ->> (List.sort ~cmp:(fun (_,a) (_,b) -> compare a b)) ->> (List.map fst)) col_map in
-  col_list, get cols, fun f -> iter f file
-    
-let make_fold file =
-  let cols,get,iter = make_iter file in
-  let fold f init =
-    let ans = ref init in
-    let g r = ans := f !ans r in
-    iter g;
-    !ans
-  in
-  cols,get,fold
 
-let hashtbl_of_file file x y =
-  let _,get,iter = make_iter file in
-  let ans = Hashtbl.create 10 in
-  let f r =
-    let x = get r x in
-    let y = get r y in
-    try
-      let prev_y = Hashtbl.find ans x in
-      if prev_y <> y then
-        failwith (sprintf "%s: previously mapped to %s but now maps to %s" x prev_y y)
-    with Not_found ->
-      Hashtbl.add ans x y
-  in
-  iter f;
-  ans
-
-let multi_hashtbl_of_file file x y =
-  let _,get,iter = make_iter file in
-  let ans = Hashtbl.create 10 in
-  let f r =
-    let x = get r x in
-    let y = get r y in
-    let prev_y = try Hashtbl.find ans x with Not_found -> StringSet.empty in
-    Hashtbl.replace ans x (StringSet.add y prev_y)
-  in
-  iter f;
-  ans
-  
-let map_of_file file x y =
-  let _,get,fold = make_fold file in
-  let f ans r =
-    let x = get r x in
-    let y = get r y in
-    try
-      let prev_y = StringMap.find x ans in
-      if prev_y <> y then
-        failwith (sprintf "%s: previously mapped to %s but now maps to %s" x prev_y y)
-      else
-        ans
-    with Not_found ->
-      StringMap.add x y ans
-  in
-  fold f StringMap.empty
-
-let multi_map_of_file file x y =
-  let _,get,fold = make_fold file in
-  let f ans r =
-    let x = get r x in
-    let y = get r y in
-    let prev_y = try StringMap.find x ans with Not_found -> StringSet.empty in
-    StringMap.add x (StringSet.add y prev_y) ans
-  in
-  fold f StringMap.empty
+  comments,
+  columns,
+  make_getter columns,
+  Enum.map (flip String.nsplit separator |- Array.of_list) e
--- src/biocaml/table.mli
+++ src/biocaml/table.mli
@@ -1,60 +1,47 @@
-(** Parsing of tabular data with named columns. Input files must be in
-    the format below. Columns must be separated by a single tab character
-    and their names must be given on the first line.    
-    {v
-    colA     colB     .     colN
-    x0A      x0B      .     x0N
-    .        .       .      .
-    .        .       .      .
-    .        .       .      .
-    xMA      xMB      .     xMN
-    v}
-    Table assumed to be rectangular even if some rows have fewer than
-    N columns. Missing column values assumed to be empty
-    string. However, any row with beyond N columns will trigger an
-    error.
+(** Tabular data. Several variations can be specified:
+
+    - Initial comment lines can be (dis)allowed (default =
+    allowed).
+
+    - The character defining the start of a comment line can be
+    specified (default = '#').
+
+    - A header row can be optionally provided giving the column
+    names. If not provided, the names are "0", "1", "2",
+    etc. (Default: no header row.)
+
+    - If there is a header row, it can be followed by another row with
+    only spaces and dashes.
+
+    - The column separator can be specified (default = "\t").
+
 *)
+
+open Batteries_uni
+
+exception Invalid of string
 
 type row
     (** Data row. *)
     
 type getter = row -> string -> string
   (** A function [get] of type [getter] can be used as [get r c] to
-      get the value of the column named [c] in row [r]. It raises
-      [Failure] if the column name is invalid, or if the given row does
-      not have a value for that column. *)
+      get the value of the column named [c] in row [r]. Raise
+      [Failure] if given row does not have a value for column [c]. *)
 
 type columns = string list
     (** All column names occurring in a file, in order that they occur. *)
 
-val make_fold : string -> (columns * getter * (('a -> row -> 'a) -> 'a -> 'a))
-  (** [make_fold file] returns the column names, a [getter] function,
-      and a fold function that can be used for the given [file]. For
-      example, the result of the code below is a list of all the values in
-      column [c].
-      {v
-      let _,get,fold = make_fold file in
-      let f l r = (get r c)::l in
-      List.rev (fold f [])
-      v}
-      Function [f] should raise [Failure] in case of error. The
-      returned [fold] function will raise [Failure] with information about
-      where the error occurred. Calling [fold] repeatedly will work
-      correctly (assuming the [file] itself does not change between
-      calls).  *)
-  
-val make_iter : string -> (columns * getter * ((row -> unit) -> unit))
-  (** Like [make_fold] but the function returned is an [iter]ator. *)
+val enum_input : ?itags:string -> IO.input -> (Comments.t option * columns * getter * row Enum.t)
+  (** Returns:
+      - comments if itags specified there are any, or None otherwise
+      - column names
+      - the getter function
+      - enumeration of the data rows
 
-val hashtbl_of_file : string -> string -> string -> (string, string) Hashtbl.t
-  (** [hashtbl_of_file file x y] returns the mapping from values of
-      column [x] to those in column [y]. Raise [Failure] if [x] maps to
-      more than one value in [y]. *)
+      Default itags is:
+      "table,comment-char=#,header,header_,separator=\t".
 
-val multi_hashtbl_of_file : string -> string -> string -> (string, StringSet.t) Hashtbl.t
-  (** [multi_hashtbl_of_file file x y] is like [hashtbl_of_file] but
-      returns mapping from [x] to set of all [y]'s it maps to. *)
-
-val map_of_file : string -> string -> string -> string StringMap.t
-val multi_map_of_file : string -> string -> string -> StringSet.t StringMap.t
-  (** Analogous to [hashtbl_of_file] and [multi_hashtbl_of_file]. *)
+      Raises [Invalid] if [input] does not conform to [itags]
+      requirements. Raises [Tags.Invalid] if [itags] are ill-formed or
+      invalid for this function. *)
