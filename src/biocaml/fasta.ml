497615b7939e9e90dbeb1c1cfb07747dd3942b45
Ashish Agarwal <agarwal1975@gmail.com>
2011-07-22 14:55:36 -0400

    Ported Fasta module to use Batteries.

--- src/biocaml/fasta.ml
+++ src/biocaml/fasta.ml
@@ -1,125 +1,39 @@
-open Sesame
-open Printf
+open Batteries_uni;; open Printf
 
-type header = string
-type t = Seq.t StringMap.t
-    (* key is header *)
+exception Error of string
 
-exception Bad of string
-let raise_bad msg = raise (Bad msg)
+type record = string * string
 
-let fold = StringMap.fold
-let iter = StringMap.iter
-let headers t = List.rev (fold (fun hdr _ ans -> hdr::ans) t [])
+let enum_input inp =
+  let e = IO.lines_of inp in
 
-let get_seq t x =
-  try StringMap.find x t
-  with Not_found -> failwith (sprintf "%s: header not found" x)
-
-module Parser = struct
-  let is_header s = 
-    if String.length s > 0
-    then s.[0] = '>'
-    else false
-
-  let header s =
-    assert (String.length s > 0);
-    assert (s.[0] = '>');
-    String.lchop s
-      
-  (* lines should point to beginning of sequence specification,
-   * returned buffer will contain valid nucleic acid sequence *)
-  let sequence lines : Buffer.t =
-    let ans = Buffer.create 10000 in
-    let go l = not (Stream.is_empty lines || is_header l) in
-    let all_are_nucleic_acid s =
-      for i = 0 to String.length s - 1 
-      do (if not (Seq.is_nucleic_acid s.[i]) then raise_bad (String.of_char s.[i]))
-      done
-    in
-    let add l =
-      try all_are_nucleic_acid l; Buffer.add_string ans l
-      with Bad msg -> raise_bad ("invalid nucleic acid " ^ msg)
-    in
-    let lines = Stream.keep_while go lines in
-    Stream.iter add lines;
-    ans
-        
-  (** next non-empty line should be header line *)
-  let section lines : (header * Seq.t) =
-    Stream.skip_while (String.for_all Char.is_space) lines;
-    let hdr = header (Stream.next lines) in
-    let seq = sequence lines in
-      (hdr, Seq.of_buffer_unsafe seq) (* ok to use unsafe because [sequence] returns valid buffer *)
-
-  let fasta file_name cin : t =
-    let lines = Stream.lines_of_channel cin in
-    let lines = Stream.map String.strip_final_cr lines in
-    let err msg = Msg.err ~pos:(Pos.fl file_name (Stream.count lines)) msg in
-    let rec loop lines ans =
-      if Stream.is_empty lines then
-        ans
-      else
-        let hdr,seq = section lines in
-        let ans =
-          if StringMap.mem hdr ans then
-            failwith (sprintf "%s: sequence with this header previously inserted" hdr)
-          else
-            StringMap.add hdr seq ans
-        in
-        loop lines ans
-    in 
-    try loop lines StringMap.empty
-    with Failure m | Bad m -> raise_bad (err m)
-end
-
-let of_file file = 
-  try_finally (Parser.fasta file) close_in (open_in file)
-
-let of_channel ic = 
-  try_finally (Parser.fasta "fasta file") close_in ic
-
-let to_file t file = 
-  let oc = open_out file in
-  let f header seq = 
-    output_string oc (">" ^ header ^ "\n");
-    output_string oc ((Seq.to_string seq) ^ "\n")
+  let open Comments in
+  let comment_char = '#' in
+  let e1,e2 = Enum.span (is_comments ~comment_char) e in
+  let comments =
+    Enum.fold (fun x y -> concat x (of_string y)) (empty comment_char) e1
   in
-  StringMap.iter f t; close_out oc
 
-(* bug-ridden -- David
-let map_headers f t = 
-  let folder hd seq acc = 
-    let newhd = f hd in
-    if StringMap.mem newhd acc then
-      StringMap.add (newhd ^ "_2") seq acc
-    else
-      StringMap.add newhd seq acc 
+  let open Enum in
+  let rec make_from_enum (e : string t) = make
+    ~next:(fun () ->
+      let a = match get e with
+        | None -> raise No_more_elements
+        | Some a -> a
+      in
+      let b = match get e with
+        | None -> Invalid "expected sequence line after '>' line but reached end-of-input" |> raise
+        | Some b -> b
+      in
+      a,b
+    )
+
+    ~count:(fun () ->
+      let n = count e in
+      if n mod 2 = 0 then n/2
+      else Invalid "underlying enum does not contain multiple of 2 records" |> raise
+    )
+
+    ~clone:(fun () -> make_from_enum (clone e))
   in
-  StringMap.fold folder t StringMap.empty
-
-let fold_file f init file = 
-  let ic = open_in file in
-  let hdrregex = Pcre.regexp ">(.+)" in
-  let acc = ref init in
-  let hdr = ref "" in
-  let seq = ref "" in
-  try 
-    while true do
-      let line = input_line ic in
-      try (
-          let arr = Pcre.extract ~rex:hdrregex line in
-          acc := f !acc !hdr (Seq.of_string !seq);
-          hdr := arr.(0);
-          seq := ""
-        )
-        with Not_found -> (seq := !seq ^ line)
-    done; assert false
-  with End_of_file -> (
-    acc := f !acc !hdr (Seq.of_string !seq);
-    close_in ic;
-    !acc
-  )
-
-let iter_file f file = fold_file (fun _ hdr seq -> f hdr seq) () file
-*)
+  comments, make_from_enum e2
--- src/biocaml/fasta.mli
+++ src/biocaml/fasta.mli
@@ -1,56 +1,10 @@
-(** Operations on FASTA files. The format for a FASTA file is shown below, but see NCBI for futher details. It consists of a set of sequence headers each followed by a sequence. See module {!Seq} for details on the format of sequences. There is no standard format for the header. Often it is the name of the sequence, but many fasta files include additional content in the header. This module treats the entire string after the initial > as the header.
+(** FASTA files. *)
+open Batteries_uni
 
-    {v
-    >header1
-    ACGT...
-    
-    >header2
-    TGAC...
-    
-    ...
-    v}
-*)
+exception Error of string
 
-type header = string
+type record = string * string
+    (** A header and a sequence. *)
 
-type t
-    (** Type of a FASTA file *)
-
-exception Bad of string
-
-val of_file : string -> t
-  (** Parse given file. Raise [Bad] if there are any parse errors. *)
-
-val of_channel : in_channel -> t
-  (** Parse given channel. Raise [Bad] if there are any parse errors. *)
-
-val to_file : t -> string -> unit
-  (** Outputs a Fasta.t to a file. *)
-
-val fold : (header -> Seq.t -> 'b -> 'b) -> t -> 'b -> 'b
-  (** [fold f t init] folds over the sequences in [t]. *)
-  
-val iter : (header -> Seq.t -> unit) -> t -> unit
-  
-val headers : t -> header list
-  (** Return all headers. *)
-  
-val get_seq : t -> header -> Seq.t
-  (** [get_seq t x] returns the sequence with header [x] in [t]. Raise [Failure] if no such sequence. *)
-
-(* bug-ridden -- David
-
-val map_headers : (string -> string) -> t -> t
-  (** [map_headers f t] performs some function f on all of the headers. Raises
-      [Failure] if some sequences are mapped to identical headers. *)
-
-val fold_file : ('a -> string -> Seq.t -> 'a) -> 'a -> string -> 'a
-  (** [fold_file f init t] performs the function [f acc header seq]
-      on each completed header * seq combination, returning the accumulator at the
-      end of the file. NOT CORRECT YET*)
-
-val iter_file : (string -> Seq.t -> unit) -> string -> unit
-  (** [iter_file f file] performs the function [f header seq] on each 
-      completed header * seq combination, returning unit. NOT CORRECT YET*)
-
-*)
+val enum_input : IO.input -> Comments.t * record Enum.t
+  (** Returns comments and enumeration of fasta records in given input. *)
