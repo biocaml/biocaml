894e803fdbf4503116c1eab4c837dd26a1bf8395
pveber <philippe.veber@gmail.com>
2012-01-28 09:50:11 +0100

    [Pwm] complete implementation in C of the naive search algorithm

--- src/lib/biocaml_pwm_stub.c
+++ src/lib/biocaml_pwm_stub.c
@@ -7,16 +7,29 @@
 #include<caml/alloc.h>
 #include<caml/misc.h>
 
-value biocaml_pwm_scan(value mat, value seq, value tol) {
-  CAMLparam3(mat, seq, tol);
+value biocaml_pwm_scan(value mat, value seq, value caml_tol) {
+  CAMLparam3(mat, seq, caml_tol);
+  CAMLlocal3(r,tmp,hit);
   int n = Wosize_val(seq);
+  int m = Wosize_val(mat);
+  double tol = Double_val(caml_tol);
   int i,j;
-
-  for(i = n - 1; i >= 0; i--) {
+  
+  r = Val_int(0); // empty list
+  for(i = n - m; i >= 0; i--) {
     float score = 0.;
-    for(j = 0; j < 17; j++) {
-      score += Double_field(Field(mat, i + j), 0);
+    for(j = 0; j < m; j++) {
+      score += Double_field(Field(mat, j), Int_val(Field(seq,i+j)));
+    }
+    if(score > tol) {
+      tmp = r;
+      hit = caml_alloc(2,0);
+      Store_field(hit,0,Val_int(i));
+      Store_field(hit,1,caml_copy_double(score));
+      r = caml_alloc(2,0);
+      Store_field(r,0,hit);
+      Store_field(r,1,tmp);
     }
   }
-  CAMLreturn (Val_unit);
+  CAMLreturn (r);
 }
