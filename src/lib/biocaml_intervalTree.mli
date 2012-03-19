bcd919cf03ce2c10e9d33aa8c0eb096ee6f1b5e8
pveber <philippe.veber@gmail.com>
2012-03-19 16:52:08 +0100

    [Interval_tree] code reordering

--- src/lib/biocaml_intervalTree.mli
+++ src/lib/biocaml_intervalTree.mli
@@ -9,25 +9,13 @@
 
 type 'a t
 
-val empty : 'a t
-(** the empty tree *)
+exception Empty_tree
+
+(** {6 Accessors} *)
 
 val is_empty : 'a t -> bool
 
-exception Empty_tree
-
-val add : int -> int -> 'a -> 'a t -> 'a t
-(** [add lo hi v t] adds the interval ([lo], [hi]) labeled with value [v] to the contents of [t]. Note that 
-    in contrast to sets, identical intervals (even with identical labels) may be *repeated* in an interval
-    tree. E.g., [add 1 2 () (add 1 2 ())] contains 2 intervals. *)
-
 val cardinal : 'a t -> int
-
-val elements : 'a t -> (int * int * 'a) list
-
-val enum : 'a t -> (int * int * 'a) BatEnum.t
-
-val backwards : 'a t -> (int * int * 'a) BatEnum.t
 
 val find_closest : int -> int -> 'a t -> int * int * 'a
 (** [find_closest lo hi t] returns the interval in [t] which is at minimal
@@ -35,6 +23,29 @@ val find_closest : int -> int -> 'a t -> int * int * 'a
     0 of each other.
 
     Raises [Empty_tree] if [t] is empty *)
+
+
+(** {6 Constructors} *)
+
+val empty : 'a t
+(** the empty tree *)
+
+val add : int -> int -> 'a -> 'a t -> 'a t
+(** [add lo hi v t] adds the interval ([lo], [hi]) labeled with value [v] to the contents of [t]. Note that 
+    in contrast to sets, identical intervals (even with identical labels) may be *repeated* in an interval
+    tree. E.g., [add 1 2 () (add 1 2 ())] contains 2 intervals. *)
+
+
+(** {6 Conversion} *)
+
+val elements : 'a t -> (int * int * 'a) list
+
+val enum : 'a t -> (int * int * 'a) BatEnum.t
+
+val backwards : 'a t -> (int * int * 'a) BatEnum.t
+
+
+(** {6 Misc} *)
 
 val print : 'a t -> unit
 (** Used for debugging purpose, should be removed in the long run *)
