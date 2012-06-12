d24fa886ea9f9235b05d15575e36c2a13a92874f
Sebastien Mondet <seb@mondet.org>
2012-06-12 15:25:21 -0400

    Test Biocaml_fastq.printer's functions

--- src/lwt_tests/main.ml
+++ src/lwt_tests/main.ml
@@ -49,6 +49,23 @@ let test_fastq_string count file =
     in
     loop ()))
 
+let reprint_fastq file =
+  let parser = Biocaml_fastq.parser () in
+  let stream_of_lines = Lwt_io.lines_of_file file in
+  let stream_of_records =
+    Lwt_stream.filter_map (fun l ->
+      Biocaml_fastq.feed_line parser l;
+      match Biocaml_fastq.next parser with
+      | `record r -> Some r
+      | _ -> None) stream_of_lines in
+  Lwt_stream.to_list stream_of_records
+  >>= fun list_of_records ->
+  let printer = Biocaml_fastq.printer () in
+  Lwt_list.iter_s (fun r ->
+    Biocaml_fastq.feed_record printer r;
+    Lwt_io.printf "%s" (Biocaml_fastq.get_string printer))
+    list_of_records
+  
   
   
 let () =
@@ -58,5 +75,8 @@ let () =
     Lwt_io.printf "\nBy pieces of %d bytes:\n" 42 >>= fun () ->
     test_fastq_string 42 Sys.argv.(1) >>= fun () ->
     Lwt_io.printf "\nBy pieces of %d bytes:\n" 4200 >>= fun () ->
-    test_fastq_string 4200 Sys.argv.(1) 
+    test_fastq_string 4200 Sys.argv.(1) >>= fun () ->
+    Lwt_io.printf "\nRe-print the FASTQ:\n" >>= fun () ->
+    reprint_fastq Sys.argv.(1)
+
   )
