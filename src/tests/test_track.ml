8aeb560c104ec55bf43289000bf8c651a4efbb19
Sebastien Mondet <seb@mondet.org>
2012-08-03 17:00:46 -0400

    Add more tests for Biocaml_track

--- src/tests/test_track.ml
+++ src/tests/test_track.ml
@@ -19,11 +19,27 @@ let test_parser () =
   test_output "browser \t hide all" (`browser (`hide `all));
   test_output "browser some other command"
     (`browser (`unknown "browser some other command"));
+  test_output "browser " (`browser (`unknown "browser "));
+  test_output "browser" (`browser (`unknown "browser"));
+
+  test_line "browser  position   chro:f42-51"  (function
+  | `error (`wrong_browser_position _) -> true
+  | _ -> false);
   
   test_output "track a=b c=d" (`track ["a", "b"; "c", "d"]);
   test_output "track   \t a=b \t   c=d \t" (`track ["a", "b"; "c", "d"]);
   test_output "track a=\"b b\" \"c\tc c\"=d"
     (`track ["a", "b b"; "c\tc c", "d"]);
+  test_output "track" (`track []);
+  test_output "track   \t" (`track []);
+
+  test_line "track a=\"b b\" \"c\tc c\"=d  \t someguyalone" (function
+  | `error (`wrong_key_value_format _) -> true
+  | _ -> false);
+  test_output "track a=\"b b\" \"c c\"="
+    (`track ["a", "b b"; "c c", ""]);
+  test_output "track a=\"b b\" \"c c\"=  o=c"
+    (`track ["a", "b b"; "c c", ""; "o", "c"]);
 
   test_output "something else" (`content "something else");
   ()
