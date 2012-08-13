74dadfd735da01ff397d5b64e42d2eb5dc4b8435
Sebastien Mondet <seb@mondet.org>
2012-08-13 12:32:34 -0400

    Switch do.ml to Core

--- do.ml
+++ do.ml
@@ -1,5 +1,10 @@
 #! /usr/bin/env ocaml
 
+#use "topfind"
+#thread
+#require "core"
+open Core.Std
+
 open Printf
 
 let command fmt =
@@ -10,10 +15,13 @@ let command fmt =
   ksprintf f fmt
 
 let remove file =
-  if Sys.file_exists file then (
-    printf "Removing %s\n%!" file;
-    Sys.remove file
-  )
+  match Sys.file_exists file with
+  | `Yes ->
+    Sys.remove file;
+    printf "Removed %s\n%!" file;
+  | `No | `Unknown -> 
+    printf "No %s to remove\n%!" file
+      
     
 let usage ch =
   fprintf ch "usage: %s {-help,-h,help,<cmd>}\n" Sys.argv.(0)
@@ -23,15 +31,15 @@ let add_help name descr = help_commands := (name, descr) :: !help_commands
 let help () =
   usage stdout;
   printf "where cmd is:\n";
-  List.iter (fun (name, descr) ->
+  List.iter (List.rev !help_commands) (fun (name, descr) ->
     printf "  %s\n    " name;
-    String.iter (function '\n' -> printf "    \n" | c -> printf "%c" c) descr;
+    String.iter ~f:(function '\n' -> printf "    \n" | c -> printf "%c" c) descr;
     printf "\n"
-  ) (List.rev !help_commands);
+  );
   ()
     
 let check_cwd () =
-  if not (Sys.file_exists "_oasis") then
+  if Sys.file_exists "_oasis" = `No then
     failwith "Wrong working directory: There is no _oasis file"
   
 let setup_clean () =
@@ -81,7 +89,7 @@ let () =
   add_help "install" "Force re-installation";
   add_help "top" "Run a toplevel with the latest built biocaml.cma";
   add_help "{test,tests}"  "Run the tests\n";
-  match List.tl (Array.to_list Sys.argv) with
+  match List.tl_exn (Array.to_list Sys.argv) with
   | [] -> usage stdout
   | "-help" :: _ | "--help" :: _ | "-h" :: _ | "help" :: _ -> help ()
   | "setup" :: [] -> setup ()
@@ -89,10 +97,10 @@ let () =
   | "clean" :: "all" :: [] -> command "make clean"; setup_clean ()
   | "configure" :: l ->
     command "ocaml setup.ml -configure %s"
-      (String.concat " " (List.map (sprintf "%S") l))
+      (String.concat ~sep:" " (List.map l (sprintf "%S")))
   | "build" :: args
   | "make" :: args ->
-    command "make %s" (String.concat " " (List.map (sprintf "%S") args))
+    command "make %s" (String.concat ~sep:" " (List.map args (sprintf "%S")))
   | "install" :: [] ->
     command "ocamlfind remove biocaml";
     command "ocaml setup.ml -reinstall";
