e0411b1f0b9081de86109e7ba15c24418499d313
Sebastien Mondet <seb@mondet.org>
2012-08-31 16:30:57 -0400

    Tags: add function guess_from_filename

--- src/lib/biocaml_tags.ml
+++ src/lib/biocaml_tags.ml
@@ -1,10 +1,26 @@
 open Biocaml_internal_pervasives
-
+open With_result
 
 type t = [
 | `gzip of t
 | `raw_zip of t
 | `gff of Biocaml_gff.tag list
+| `bam
+| `sam
 ] with sexp
 
   
+let rec guess_from_filename filename =
+  match Filename.split_extension filename with
+  | (f, Some "gz") ->
+    guess_from_filename f
+    >>= fun t ->
+    return (`gzip t)
+  | (_, Some term) ->
+    begin match term with
+    | "gff" -> return (`gff Biocaml_gff.default_tags)
+    | "bam" -> return `bam
+    | "sam" -> return `sam
+    | u -> fail (`extension_unknown u)
+    end
+  | (_, None) -> fail (`extension_absent)
--- src/lib/biocaml_tags.mli
+++ src/lib/biocaml_tags.mli
@@ -3,4 +3,11 @@ type t = [
 | `gzip of t
 | `raw_zip of t
 | `gff of Biocaml_gff.tag list
+| `bam
+| `sam
 ] with sexp
+
+val guess_from_filename: string ->
+  (t, [> `extension_absent | `extension_unknown of string ]) Core.Result.t
+
+  
