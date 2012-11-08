53fc5caddbac811a8183b76c39393f8815926cf1
Sebastien Mondet <seb@mondet.org>
2012-11-08 12:06:32 -0500

    Bar: convert to Internal_pervasives

--- src/lib/biocaml_bar.ml
+++ src/lib/biocaml_bar.ml
@@ -1,26 +1,31 @@
-open Biocaml_std
+open Biocaml_internal_pervasives
+
+module Msg = Biocaml_msg
+module Pos = Biocaml_pos
 
 type header = (string * string) list
-    (* list of tag-value pairs *)
+(* list of tag-value pairs *)
 
-type section =
-    {
-      sec_num:int;
-      sec_name:string;
-      sec_data:(int * float) list (* stored in ascending order by int *)
-    }
+type section = {
+  sec_num:int;
+  sec_name:string;
+  sec_data:(int * float) list (* stored in ascending order by int *)
+}
 
 type t = header * section list (* section list in ascending order by sec_name *)
 exception Bad of string
 let raise_bad msg = raise (Bad msg)
     
+
+let (<--) a b = fun x -> a (b x)
+let get_assoc_exn a l = List.Assoc.find_exn l a
 let num_sections = List.length <-- snd
-let data_type = List.assoc_exn "Data" <-- fst
-let scale = List.assoc_exn "Scale" <-- fst
-let genomic_map = List.assoc_exn "Genomic_Map" <-- fst
-let alg_name = List.assoc_exn "AlgName" <-- fst
-let alg_version = List.assoc_exn "AlgVersion" <-- fst
-let coord_convention = List.assoc_exn "probe_coordinate_convention" <-- fst
+let data_type = get_assoc_exn "Data" <-- fst
+let scale = get_assoc_exn "Scale" <-- fst
+let genomic_map = get_assoc_exn "Genomic_Map" <-- fst
+let alg_name = get_assoc_exn "AlgName" <-- fst
+let alg_version = get_assoc_exn "AlgVersion" <-- fst
+let coord_convention = get_assoc_exn "probe_coordinate_convention" <-- fst
 let sections = snd
 
 let section (_,secs) nm =
@@ -36,9 +41,9 @@ let sectioni (_,secs) i =
 let to_list (_,sections) =
   let f s =
     let chr = s.sec_name in
-    List.map (fun (i,v) -> chr,i,v) s.sec_data
+    List.map ~f:(fun (i,v) -> chr,i,v) s.sec_data
   in
-  List.flatten (List.map f sections)      
+  List.concat (List.map ~f sections)      
       
 module Parser = struct
   let junk_blank_lines lines =
@@ -50,27 +55,29 @@ module Parser = struct
     | [t; v] -> (t, v)
     | _ -> raise_bad (sprintf "invalid tag-value pair %s" s')
       
-  (* lines should point to beginning of file, upon return will point to start of first section *)
+  (* lines should point to beginning of file, upon return will point
+     to start of first section *)
   let header lines =
     let lines' = Stream.keep_while
-                   (fun s -> not (String.for_all Char.is_whitespace s)) lines in
+      (fun s -> not (String.for_all ~f:Char.is_whitespace s)) lines in
     let f accum l = (tag_value l)::accum in
     let ans = List.rev (Stream.fold f [] lines') in
-      junk_blank_lines lines; ans
-
-  (* lines should point to start of a section, upon return will point to start of next section or end of file *)
+    junk_blank_lines lines; ans
+      
+  (* lines should point to start of a section, upon return will point
+     to start of next section or end of file *)
   let section lines =
-    let tv lines = snd (tag_value (Stream.next lines)) in
+    let tv lines = snd (tag_value (Stream.next_exn lines)) in
     let seq_num = int_of_string (tv lines) in
     let seq_name = tv lines in
     let num_hits = int_of_string (tv lines) in
     junk_blank_lines lines;
 
     let lines' = Stream.keep_while
-                   (fun s -> not (String.for_all Char.is_whitespace s)) lines in
+                   (fun s -> not (String.for_all ~f:Char.is_whitespace s)) lines in
     let parse_line s =
       match String.split s '\t' with
-      | [i; f] -> (int_of_string i, float_of_string f)
+      | [i; f] -> (Int.of_string i, Float.of_string f)
       | _ -> raise_bad ("data row must contain exactly two fields")
     in
     let data = Stream.to_list (Stream.map parse_line lines') in
@@ -83,14 +90,14 @@ module Parser = struct
          
   let of_file file =
     let of_channel cin =
-      let lines = Stream.map String.strip_final_cr (Stream.lines_of_channel cin) in
+      let lines = Stream.map String.rstrip (Stream.lines_of_channel cin) in
       let err msg = Msg.err ~pos:(Pos.fl file (Stream.count lines)) msg in
         try
           let hdr = header lines in
           let secs = ref [] in
           let _ = while not (Stream.is_empty lines) do secs := (section lines)::!secs done in
           let secs = List.sort ~cmp:(fun s1 s2 -> Pervasives.compare s1.sec_name s2.sec_name) !secs in
-          let expected_num_secs = int_of_string (List.assoc_exn "Number Sequences" hdr) in
+          let expected_num_secs = int_of_string (get_assoc_exn "Number Sequences" hdr) in
           let actual_num_secs = List.length secs in
             if actual_num_secs = expected_num_secs then
               (hdr,secs)
@@ -99,7 +106,7 @@ module Parser = struct
         with
             Failure msg | Bad msg -> raise_bad (err msg)
     in
-    try_finally_exn of_channel ~fend:close_in (open_in file)
+    try_finally_exn of_channel ~fend:In_channel.close (open_in file)
 end
   
 let of_file = Parser.of_file
