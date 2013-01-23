8dee3b9527a8b723ba62cd5736cc6015708f391b
Ashish Agarwal <agarwal1975@gmail.com>
2013-01-23 17:23:18 -0500

    copy contents of Transform.Line_oriented to Lines
    Mostly a verbatim copy, but slight restructuring and renaming.

--- src/lib/biocaml_lines.ml
+++ src/lib/biocaml_lines.ml
@@ -1,4 +1,5 @@
 open Biocaml_internal_pervasives
+module Transform = Biocaml_transform
 
 type item = string
 
@@ -12,3 +13,116 @@ let string_to_items s =
     | None -> assert false
     | Some "" -> List.take lines (n - 1), true
     | Some _ -> lines, false
+
+module Buffer = struct
+
+  type t = {
+    mutable unfinished_line : string option;
+    lines : string Queue.t;
+    mutable parsed_lines : int;
+    filename : string option;
+  }
+
+  let parsing_buffer ?filename () =
+    {unfinished_line = None;
+     lines = Queue.create ();
+     parsed_lines = 0;
+     filename}
+
+  let feed_line p s =
+    Queue.enqueue p.lines s
+
+  let feed_string p s =
+    let lines = String.split s ~on:'\n' in
+    let rec faux = function
+      | [] -> assert false
+      | [ "" ] -> (* last char was a "\n" *) ()
+      | [ s ] -> (* there is a partial line at the end *)
+        p.unfinished_line <- Some s;
+      | h :: t ->
+        Queue.enqueue p.lines h;
+        faux t
+    in
+    match p.unfinished_line, lines with
+    | _, [] -> assert false
+    | _, [""] -> ()
+    | None, l -> faux l
+    | Some s, h :: t ->
+      p.unfinished_line <- None;
+      faux ((s ^ h) :: t)
+
+  let queued_lines p = Queue.length p.lines
+
+  let next_line p =
+    let l = Queue.dequeue p.lines in
+    if l <> None then (
+      p.parsed_lines <- p.parsed_lines + 1;
+    );
+    l
+
+  exception No_next_line
+
+  let next_line_exn p =
+    match next_line p with
+    | Some s -> s
+    | None -> raise No_next_line
+
+  let current_position p =
+    Biocaml_pos.make ?file:p.filename ~line:p.parsed_lines ()
+
+  let is_empty p =
+    Queue.is_empty p.lines && p.unfinished_line = None
+
+  let contents p = Queue.to_list p.lines, p.unfinished_line
+
+  let empty p = (Queue.clear p.lines; p.unfinished_line <- None)
+
+  let make ?name ?filename ~next ~on_error () =
+    let lo_parser = parsing_buffer ?filename () in
+    Transform.make ?name ()
+      ~feed:(feed_string lo_parser)
+      ~next:(fun stopped ->
+        match next lo_parser with
+        | `output (Ok r) -> `output (Ok r)
+        | `output (Error r) -> `output (Error (on_error (`next r)))
+        | `not_ready ->
+          if stopped then (
+            if is_empty lo_parser then
+              `end_of_stream
+            else
+              let l,o = contents lo_parser in
+              `output
+                (Error
+                    (on_error
+                        (`incomplete_input (current_position lo_parser, l, o))))
+          ) else
+            `not_ready)
+
+  let make_merge_error =
+    make
+      ~on_error:(function
+        | `next e -> e
+        | `incomplete_input e -> `incomplete_input e)
+
+end
+
+let lines () =
+  let buf = Buffer.parsing_buffer () in
+  Transform.make ~name:"lines"
+    ~feed:(Buffer.feed_string buf)
+    ~next:(function
+      | true -> (match Buffer.next_line buf with
+          | Some line -> `output line
+          | None -> (match Buffer.contents buf with
+              | [], None -> `end_of_stream
+              | [], Some unfinished_line ->
+                  (Buffer.empty buf; `output unfinished_line)
+              | _ -> assert false
+            )
+        )
+      | false -> (match Buffer.next_line buf with
+          | None -> `not_ready
+          | Some line -> `output line
+        )
+    )
+    ()
--- src/lib/biocaml_lines.mli
+++ src/lib/biocaml_lines.mli
@@ -1,7 +1,6 @@
 (** Lines of a file. The end of a line is indicated by a single
     newline character. *)
 
-
 (** A single line, possibly empty, without the ending newline
     character. *)
 type item = private string
@@ -10,3 +9,81 @@ type item = private string
     the resuling list of lines. The returned bool is true if the final
     line ended with a newline or false otherwise. *)
 val string_to_items : string -> (item list * bool)
+
+(** Return a transform that converts a stream of arbitrary strings to
+    a stream of lines. If the input terminates without a newline,
+    the trailing string is still considered a line. *)
+val lines : unit -> (string, string) Biocaml_transform.t
+
+
+(** Buffer of lines. *)
+module Buffer : sig
+
+  type t
+
+  (** The exception thrown by [next_line_exn]. *)
+  exception No_next_line
+
+  (** Create a "parser"; the optional [filename] is used only to
+      create error locations. *)
+  val parsing_buffer: ?filename:string -> unit -> t
+
+  (** Feed the parser with a line. *)
+  val feed_line: t -> string -> unit
+
+  (** Feed the parser with an arbitrary string buffer. *)
+  val feed_string: t -> string -> unit
+
+  (** Get the number of lines ready-to-use in the buffer/queue. *)
+  val queued_lines: t -> int
+
+  (** Tell if the parser's buffers are empty or not. For instance,
+      when there is no more content to feed and [next_line] returns
+      [None], [is_empty p = true] means that the content did not end
+      with a complete line. *)
+  val is_empty: t -> bool
+
+  (** Get the next line. *)
+  val next_line: t -> string option
+
+  (** Get the next line, but throw [No_next_line] if there is no line
+      to return. *)
+  val next_line_exn: t -> string
+
+  (** Get the current position in the stream. *)
+  val current_position: t -> Biocaml_pos.t
+
+  (** Return any remaining lines and the unfinished string, without
+      removing them from the buffer. *)
+  val contents : t -> string list * string option
+
+  (** Empty the buffer. Subsequent call to [contents] will return
+      [(\[\], None)]. *)
+  val empty : t -> unit
+
+  (** Build a stoppable line-oriented parsing_buffer. *)
+  val make : ?name:string -> ?filename:string ->
+    next:(t ->
+      [ `not_ready | `output of ('b, 'errnext) Core.Result.t ]) ->
+    on_error:(
+      [`next of 'errnext
+      | `incomplete_input of Biocaml_pos.t * string list * string option] ->
+        'err) ->
+    unit ->
+    (string, ('b, 'err) Core.Result.t) Biocaml_transform.t
+
+  (** Do like [make] but merge [`incomplete_input _] with the
+      errors of [~next] (which must be polymorphic variants). *)
+  val make_merge_error :
+    ?name:string ->
+    ?filename:string ->
+    next:(t ->
+      [ `not_ready
+      | `output of ('a,
+                   [> `incomplete_input of
+                     Biocaml_pos.t * string list * string option ]
+                     as 'b) Core.Result.t ]) ->
+    unit ->
+    (string, ('a, 'b) Core.Result.t) Biocaml_transform.t
+
+end
