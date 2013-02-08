f9b9428c7ec5c2aafc3441109e28c94c1e2d1201
Ashish Agarwal <agarwal1975@gmail.com>
2013-02-08 11:39:42 -0500

    api doc: use custom intro file
    New ocamlbuild configuration is based on Batteries. I'm not sure this
    is the best solution, but it works.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -4,10 +4,20 @@
 
 open Ocamlbuild_plugin
 
+let doc_intro = "src/doc/intro.txt"
+let doc_title = "Biocaml API Reference"
+
 let mydispatch = function
   | After_options ->
      Options.ocamldoc := S[!Options.ocamldoc;
                            A"-thread"; A"-charset"; A"utf-8"; A"-sort" ]
+  | After_rules ->
+    dep ["ocaml"; "doc"; "extension:html"] & [doc_intro];
+    flag ["ocaml"; "doc"; "extension:html"] &
+      (S[
+        A"-t"; A doc_title;
+        A"-intro"; P doc_intro;
+      ])
   | _ -> ()
 
 let () =
