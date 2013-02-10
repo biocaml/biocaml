1a8fa9f3801d33d9c8c591610e8efd4bcb70bd8b
pveber <philippe.veber@gmail.com>
2013-02-10 21:42:54 +0100

    iset: corification

--- src/lib/biocaml_iset.ml
+++ src/lib/biocaml_iset.ml
@@ -190,39 +190,39 @@ let rec add s n =
       make_tree s0 (v1, n) s1
   else s
 
-let rec from n s =
+let rec from s ~n =
   if is_empty s then empty else
   let (v1, v2) as v = root s in
   let s0 = left_branch s in
   let s1 = right_branch s in
-  if n < v1 then make_tree (from n s0) v s1 else
-  if n > v2 then from n s1 else
+  if n < v1 then make_tree (from s0 ~n) v s1 else
+  if n > v2 then from s1 ~n else
   make_tree empty (n, v2) s1
 
-let after n s = if n = max_int then empty else from (n + 1) s
+let after s ~n = if n = max_int then empty else from s ~n:(n + 1)
 
-let rec until n s =
+let rec until s ~n =
   if is_empty s then empty else
   let (v1, v2) as v = root s in
   let s0 = left_branch s in
   let s1 = right_branch s in
-  if n > v2 then make_tree s0 v (until n s1) else
-  if n < v1 then until n s0 else
+  if n > v2 then make_tree s0 v (until s1 ~n) else
+  if n < v1 then until s0 ~n else
   make_tree s0 (v1, n) empty
 
-let before n s = if n = min_int then empty else until (n - 1) s
+let before s ~n = if n = min_int then empty else until s ~n:(n - 1)
 
 let add_range s n1 n2 =
   if n1 > n2 then invalid_arg (Printf.sprintf "ISet.add_range - %d > %d" n1 n2) else
   let n1, l =
     if n1 = min_int then n1, empty else
-    let l = until (n1 - 1) s in
+    let l = until s ~n:(n1 - 1) in
     if is_empty l then n1, empty else
     let (v1, v2), l' = split_rightmost l in
     if v2 + 1 = n1 then v1, l' else n1, l in
   let n2, r =
     if n2 = max_int then n2, empty else
-    let r = from (n2 + 1) s in
+    let r = from s ~n:(n2 + 1) in
     if is_empty r then n2, empty else
     let (v1, v2), r' = split_leftmost r in
     if n2 + 1 = v1 then v2, r' else n2, r in
@@ -230,17 +230,12 @@ let add_range s n1 n2 =
 
 let singleton n = singleton_tree (n, n)
 
-(*$T singleton
-  singleton 3 |> mem 3
-  singleton 3 |> mem 4 |> not
- *)
-
-let rec remove n s =
+let rec remove s n =
   if is_empty s then empty else
   let (v1, v2) as v = root s in
   let s1 = left_branch s in
   let s2 = right_branch s in
-  if n < v1 then make_tree (remove n s1) v s2
+  if n < v1 then make_tree (remove s1 n) v s2
   else if n = v1 then
     if v1 = v2 then concat s1 s2 else
     make_tree s1 (v1 + 1, v2) s2
@@ -248,26 +243,11 @@ let rec remove n s =
     let s = make_tree s1 (v1, n - 1) empty in
     make_tree s (n + 1, v2) s2
   else if n = v2 then make_tree s1 (v1, v2 - 1) s2 else
-  make_tree s1 v (remove n s2)
+  make_tree s1 v (remove s2 n)
 
-(*$= remove & ~cmp:equal ~printer:(IO.to_string print)
-  empty (remove 3 (singleton 3))
-  (of_list [1,5] |> remove 5) (of_list [1,4])
-  (of_list [1,5] |> remove 1) (of_list [2,5])
-  (of_list [1,5] |> remove 3) (of_list [1,2;4,5])
-  (of_list [4,6;1,3;8,10] |> remove 1) (of_list [2,3;4,6;8,10])
-  (of_list [4,6;1,3;8,10] |> remove 10) (of_list [1,3;4,6;8,9])
- *)
-
-let remove_range n1 n2 s =
+let remove_range s n1 n2 =
   if n1 > n2 then invalid_arg "ISet.remove_range" else
-  concat (before n1 s) (after n2 s)
-
-(*$= remove_range & ~cmp:equal ~printer:(IO.to_string print)
-  empty (remove_range 10 15 (of_list [10,15]))
-  (of_list [0,20] |> remove_range 3 5) (of_list [0,2;6,20])
-  (of_list [0,20] |> remove_range 3 5 |> remove_range 8 10 |> remove_range 5 8) (of_list [0,2;11,20])
- *)
+  concat (before s n1) (after s n2)
 
 let rec union s1 s2 =
   if is_empty s1 then s2 else
@@ -276,8 +256,8 @@ let rec union s1 s2 =
   let n1, n2 = root s1 in
   let l1 = left_branch s1 in
   let r1 = right_branch s1 in
-  let l2 = before n1 s2 in
-  let r2 = after n2 s2 in
+  let l2 = before s2 ~n:n1 in
+  let r2 = after s2 n2 in
   let n1, l =
     if n1 = min_int then n1, empty else
     let l = union l1 l2 in
@@ -307,9 +287,9 @@ let rec inter s1 s2 =
   let n1, n2 = root s1 in
   let l1 = left_branch s1 in
   let r1 = right_branch s1 in
-  let l2 = before n1 s2 in
-  let r2 = after n2 s2 in
-  let m = until n2 (from n1 s2) in
+  let l2 = before s2 n1 in
+  let r2 = after s2 n2 in
+  let m = until (from s2 n1) n2 in
   concat (concat (inter l1 l2) m) (inter r1 r2)
 
 (*$= inter & ~cmp:equal ~printer:(IO.to_string print)
@@ -358,58 +338,32 @@ let compare s1 s2 = compare_aux [`Set s1] [`Set s2]
 
 let equal s1 s2 = compare s1 s2 = 0
 
-(*$T equal
-  not (equal (of_list [3,3;5,5]) (of_list [3,3;1,1]))
- *)
-
-let ord = BatOrd.ord compare
-
 let rec subset s1 s2 =
   if is_empty s1 then true else
   if is_empty s2 then false else
   let v1, v2 = root s2 in
   let l2 = left_branch s2 in
   let r2 = right_branch s2 in
-  let l1 = before v1 s1 in
-  let r1 = after v2 s1 in
+  let l1 = before s1 v1 in
+  let r1 = after s1 v2 in
   (subset l1 l2) && (subset r1 r2)
 
-(*$T subset
-  subset (of_list [1,3]) (of_list [1,5])
-  subset (of_list [1,3]) (of_list [1,3])
-  subset (of_list []) (of_list [1,5])
-  not (subset (of_list [0,3]) (of_list [1,5]))
-  not (subset (of_list [0,6]) (of_list [1,5]))
- *)
+let fold_range s ~init ~f = BatAvlTree.fold (fun (n1, n2) x -> f n1 n2 x) s init
 
-let fold_range f s x0 = BatAvlTree.fold (fun (n1, n2) x -> f n1 n2 x) s x0
-
-let fold f s x0 =
+let fold s ~init ~f =
   let rec g n1 n2 a =
     if n1 = n2 then f n1 a else
     g (n1 + 1) n2 (f n1 a) in
-  fold_range g s x0
+  fold_range ~f:g s ~init
 
-(*$= fold & ~cmp:Int.equal ~printer:string_of_int
-  (fold (+) (of_list [1,3]) 0) 6
- *)
+let iter s ~f = fold s ~init:() ~f:(fun n () -> f n)
 
-let iter proc s = fold (fun n () -> proc n) s ()
+let iter_range s ~f = BatAvlTree.iter (fun (n1, n2) -> f n1 n2) s
 
-(*$T iter
-  let a = ref 0 in iter (fun _ -> incr a) (of_list [1,3;5,8]); !a = 7
- *)
-
-let iter_range proc = BatAvlTree.iter (fun (n1, n2) -> proc n1 n2)
-
-(*$T iter_range
-  let a = ref 0 in iter_range (fun _ _ -> incr a) (of_list [1,3;5,8]); !a = 2
- *)
-
-let for_all p s =
+let for_all s ~f =
   let rec test_range n1 n2 =
-    if n1 = n2 then p n1 else
-    p n1 && test_range (n1 + 1) n2 in
+    if n1 = n2 then f n1 else
+    f n1 && test_range (n1 + 1) n2 in
   let rec test_set s =
     if is_empty s then true else
     let n1, n2 = root s in
@@ -423,10 +377,10 @@ let for_all p s =
   not (for_all (fun x -> x = 5) (of_list [4,5]))
  *)
 
-let exists p s =
+let exists s ~f =
   let rec test_range n1 n2 =
-    if n1 = n2 then p n1 else
-    p n1 || test_range (n1 + 1) n2 in
+    if n1 = n2 then f n1 else
+    f n1 || test_range (n1 + 1) n2 in
   let rec test_set s =
     if is_empty s then false else
     let n1, n2 = root s in
@@ -455,11 +409,7 @@ let filter_range p n1 n2 a =
 	  loop (n1 + 1) n2 (make_tree a (v1, n1 - 1) empty) None in
   loop n1 n2 a None
 
-let filter p s = fold_range (filter_range p) empty s
-
-(*$T filter
-  true || equal (filter (fun x -> x <> 5) (of_list [1,10])) (of_list [1,4;6,10])
- *)
+let filter s ~f = fold_range s ~f:(filter_range f) ~init:empty
 
 let partition_range p n1 n2 (a, b) =
   let rec loop n1 n2 acc =
@@ -478,17 +428,17 @@ let partition_range p n1 n2 (a, b) =
       loop (n1 + 1) n2 acc in
   loop n1 n2 (a, b, (p n1, n1))
 
-let partition p s = fold_range (partition_range p) s (empty, empty)
+let partition s ~f = fold_range ~f:(partition_range f) s ~init:(empty, empty)
 
 let cardinal s =
-  fold_range (fun n1 n2 c -> c + n2 - n1 + 1) s 0
+  fold_range ~f:(fun n1 n2 c -> c + n2 - n1 + 1) s ~init:0
 
 (*$T cardinal
   cardinal (of_list [1,3;5,9]) = 8
  *)
 
 let rev_ranges s =
-  fold_range (fun n1 n2 a -> (n1, n2) :: a) s []
+  fold_range ~f:(fun n1 n2 a -> (n1, n2) :: a) s ~init:[]
 
 let rec burst_range n1 n2 a =
   if n1 = n2 then n1 :: a else
--- src/lib/biocaml_iset.mli
+++ src/lib/biocaml_iset.mli
@@ -35,10 +35,10 @@ val intersects_range : t -> int -> int -> bool
 val singleton : int -> t
 (** Return the singleton set containing only the given element *)
 
-val remove : int -> t -> t
+val remove : t -> int -> t
 (** Remove an element from the given set, returning a new set *)
 
-val remove_range : int -> int -> t -> t
+val remove_range : t -> int -> int -> t
 (** [remove_range lo hi t] removes a range of elements from the given set, returning a new set
     @raise Invalid_argument if [lo] > [hi] *)
 
@@ -69,50 +69,46 @@ val equal : t -> t -> bool
     polymorphic (=) on these sets, as the same set can have multiple
     representations depending on how it was built. *)
 
-val ord : t -> t -> BatOrd.order
-(** Same as [compare] but returns [BatOrd.Lt | BatOrd.Eq | BatOrd.Gt]
-    instead of an int. *)
-
 val subset : t -> t -> bool
 (** [subset t u] returns [true] if [t] is a subset of [u] *)
 
-val from : int -> t -> t
-(** [from x t] returns the portion of [t] in the range [x, max_int] *)
+val from : t -> n:int -> t
+(** [from ~n t] returns the portion of [t] in the range [n, max_int] *)
 
-val after : int -> t -> t
-(** [after x t] returns the portion of [t] in the range [x+1, max_int] *)
+val after : t -> n:int -> t
+(** [after ~n t] returns the portion of [t] in the range [n+1, max_int] *)
 
-val until : int -> t -> t
-(** [until x t] returns the portion of [t] in the range [min_int, x] *)
+val until : t -> n:int -> t
+(** [until ~n t] returns the portion of [t] in the range [min_int, n] *)
 
-val before : int -> t -> t
-(** [before x t] returns the portion of [t] in the range [min_int, x-1] *)
+val before : t -> n:int -> t
+(** [before x t] returns the portion of [t] in the range [min_int, n-1] *)
 
-val iter : (int -> unit) -> t -> unit
+val iter : t -> f:(int -> unit) -> unit
 (** [iter f t] calls [f] once for each element of [t] *)
 
-val iter_range : (int -> int -> unit) -> t -> unit
-(** [iter_range f t] calls [f] once for each contiguous range of [t].
+val iter_range : t -> f:(int -> int -> unit) -> unit
+(** [iter_range ~f t] calls [f] once for each contiguous range of [t].
     The contiguous ranges of a set are sequences of adjacent integers
     all part of the set. *)
 
-val fold : (int -> 'a -> 'a) -> t -> 'a -> 'a
+val fold : t -> init:'a -> f:(int -> 'a -> 'a) -> 'a
 (** [fold f t x0] returns the final result of merging each element of
     [t] into [x0] using merge function [f] *)
 
-val fold_range : (int -> int -> 'a -> 'a) -> t -> 'a -> 'a
+val fold_range : t -> init:'a -> f:(int -> int -> 'a -> 'a) -> 'a
 (** As fold, but operates on contiguous ranges *)
 
-val for_all : (int -> bool) -> t -> bool
+val for_all : t -> f:(int -> bool) -> bool
 (** Tests whether a predicate applies to all elements of the set *)
 
-val exists : (int -> bool) -> t -> bool
+val exists : t -> f:(int -> bool) -> bool
 (** Test whether some element of a set satisfies a predicate *)
 
-val filter : (int -> bool) -> t -> t
+val filter : t -> f:(int -> bool) -> t
 (** Builds the subset of those elements that satisfy the predicate *)
 
-val partition : (int -> bool) -> t -> t * t
+val partition : t -> f:(int -> bool) -> t * t
 (** partitions the input set into two sets with elements that satisfy
     the predicate and those that don't *)
 
