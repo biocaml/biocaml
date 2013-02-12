9e26d1efefa3b87cc533c0fabc14c2e798b2cace
Sebastien Mondet <seb@mondet.org>
2013-02-12 16:06:35 -0500

    Table: create new module with Row.of_line

---
+++ src/lib/biocaml_table.mli
@@ -0,0 +1,17 @@
+
+module Row : sig
+  type item = [`int of int | `float of float | `string of string ]
+
+  type t = item array
+
+  type item_type = [`type_int | `type_float | `type_string ]
+
+  type t_type = item_type array
+
+  val of_line: ?separators:char list -> format:t_type -> Biocaml_line.t ->
+    (t, [> `wrong_format of
+             [> `column_number
+             | `float_of_string of string
+             | `int_of_string of string ] * t_type * string ]) Core.Result.t
+end
+
