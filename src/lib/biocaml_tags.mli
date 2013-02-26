f15a0aed7f4e20c46448f1ccaf87bf1b93b27864
Sebastien Mondet <seb@mondet.org>
2013-02-26 16:42:17 -0500

    Tags: improve documentation

--- src/lib/biocaml_tags.mli
+++ src/lib/biocaml_tags.mli
@@ -1,3 +1,5 @@
+(** Meta-information about files and their formats. *)
+
 
 type t = [
 | `gzip of t
@@ -10,14 +12,24 @@ type t = [
 | `fastq
 | `table of char
 | `fasta of [`int | `char | `unknown ]
-] with sexp
+]
+(** Description of file formats. *)
 
 val default_extension: t -> string
+(** Get a filename extension for a given
+    format. E.g. [default_extension (`gzip (`fasta `char))] will be
+    ["fasta.gz"]. *)
 
 val guess_from_filename: string ->
   (t, [> `extension_absent | `extension_unknown of string ]) Core.Result.t
-
+(** Get a tag as precise as possible for a given filename. *)
 
 val of_string: string -> (t, [> `parse_tags of exn]) Core.Result.t
+(** Parse a tag specification (the format is, for now, based on
+    S-Expressions, but this will change). *)
+
 val to_string: t -> string
+(** Convert a [t]ag to its string specification. *)
+
+include Core.Sexpable.S with type t := t
 
