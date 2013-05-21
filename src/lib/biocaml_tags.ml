56cf4d8ea8f034a5fad95e9e8f368128313e9fb0
Sebastien Mondet <seb@mondet.org>
2013-05-21 11:44:36 -0400

    Tags: add sub-module `Output_transform` (#102)
    Most of the code is imported from `App_common`.

--- src/lib/biocaml_tags.ml
+++ src/lib/biocaml_tags.ml
@@ -65,3 +65,111 @@ let of_string s =
 let to_string t =
   let open Sexplib in
   sexp_of_t t |! Sexp.to_string_hum
+
+module Output_transform = struct
+
+  type sam_output_error = [
+    | `bam of Biocaml_bam.Error.item_to_raw
+    | `sam of Biocaml_sam.Error.item_to_raw
+  ] with sexp_of
+
+  type tags = t
+
+  (** Generic union of possible output transforms. *)
+  type t = [
+    | `to_sam_item of
+        (Biocaml_sam.item, (string, sam_output_error) Result.t)
+          Biocaml_transform.t
+    | `to_gff of(Biocaml_gff.item, string) Biocaml_transform.t
+    | `to_wig of (Biocaml_wig.item, string) Biocaml_transform.t
+    | `to_bed of (Biocaml_bed.item, string) Biocaml_transform.t
+    | `to_fastq of (Biocaml_fastq.item, string) Biocaml_transform.t
+    | `to_char_fasta of
+        (Biocaml_fasta.char_seq Biocaml_fasta.raw_item, string)
+          Biocaml_transform.t
+    | `to_int_fasta of
+        (Biocaml_fasta.int_seq Biocaml_fasta.raw_item, string)
+          Biocaml_transform.t
+    | `to_table of (Biocaml_table.Row.t, string) Biocaml_transform.t
+  ]
+
+  let name = function
+  | `to_sam_item _ -> "to_sam_item"
+  | `to_gff _ -> "to_gff"
+  | `to_wig _ -> "to_wig"
+  | `to_bed _ -> "to_bed"
+  | `to_fastq _ -> "to_fastq"
+  | `to_char_fasta _ -> "to_char_fasta"
+  | `to_int_fasta _ -> "to_int_fasta"
+  | `to_table _ -> "to_table"
+
+  let from_tags ?zip_level ?zlib_buffer_size
+      (output_tags: tags) : (t, _) Result.t =
+    let rec output_transform ?with_zip (output_tags : file_format) =
+      let with_zip_result t =
+        match with_zip with
+        | Some z -> Biocaml_transform.compose_result_left t z
+        | None -> t
+      in
+      let with_zip_no_error t =
+        match with_zip with
+        | Some z -> Biocaml_transform.compose t z
+        | None -> t
+      in
+      let to_sam_item t = return (`to_sam_item (with_zip_result t) : t) in
+      match output_tags with
+      | `raw_zip (tags: file_format) ->
+        output_transform
+          ~with_zip:(Biocaml_zip.Transform.zip ?zlib_buffer_size
+                       ~format:`raw ?level:zip_level ()) tags
+      | `gzip (tags: file_format) ->
+        output_transform
+          ~with_zip:(Biocaml_zip.Transform.zip ?zlib_buffer_size
+                       ~format:`gzip ?level:zip_level ()) tags
+      | `bam ->
+        to_sam_item (
+          Biocaml_transform.compose_result_left
+            (Biocaml_transform.on_output
+               (Biocaml_bam.Transform.item_to_raw ())
+               (function Ok o -> Ok o | Error e -> Error (`bam e)))
+            (Biocaml_bam.Transform.raw_to_string ?gzip_level:zip_level
+               ?zlib_buffer_size ()))
+      | `sam ->
+        to_sam_item (
+          Biocaml_transform.compose_result_left
+            (Biocaml_transform.on_output
+               (Biocaml_sam.Transform.item_to_raw ())
+               (function Ok o -> Ok o | Error e -> Error (`sam e)))
+            (Biocaml_sam.Transform.raw_to_string ()))
+      | `gff tag_list ->
+        let t = Biocaml_gff.Transform.item_to_string ~tags:tag_list () in
+        return (`to_gff (with_zip_no_error t) : t)
+      | `wig tag_list ->
+        let t = Biocaml_wig.Transform.item_to_string  ~tags:tag_list () in
+        return (`to_wig (with_zip_no_error t) : t)
+      | `bed ->
+        let t = Biocaml_bed.Transform.item_to_string  () in
+        return (`to_bed (with_zip_no_error t) : t)
+      | `fastq ->
+        let t = Biocaml_fastq.Transform.item_to_string () in
+        return (`to_fastq (with_zip_no_error t) : t)
+      | `fasta (`char_sequence _ as tags) ->
+        (* TODO output warning? if `unknown *)
+        let t = Biocaml_fasta.Transform.char_seq_raw_item_to_string ~tags () in
+        return (`to_char_fasta (with_zip_no_error t) : t)
+      | `fasta (`int_sequence _ as tags) ->
+        let t = Biocaml_fasta.Transform.int_seq_raw_item_to_string ~tags () in
+        return (`to_int_fasta (with_zip_no_error t) : t)
+      | `table tags ->
+        let t =
+          Biocaml_transform.compose
+            (Biocaml_table.Row.Transform.item_to_line ~tags ())
+            (Biocaml_lines.Transform.item_to_string ()) in
+        return (`to_table (with_zip_no_error t) : t)
+    in
+    match output_tags with
+    | `list (tags : tags list) -> fail (`not_implemented "list output_tags")
+    | #file_format as file_output_tags -> output_transform file_output_tags
+
+
+end
--- src/lib/biocaml_tags.mli
+++ src/lib/biocaml_tags.mli
@@ -40,9 +40,55 @@ val of_string: string -> (t, [> `parse_tags of exn]) Core.Result.t
 val to_string: t -> string
 (** Convert a [t]ag to its string specification. *)
 
+(** {2 Transforms From Tags } *)
+
+
+module Output_transform: sig
+
+
+  type sam_output_error = [
+    | `bam of Biocaml_bam.Error.item_to_raw
+    | `sam of Biocaml_sam.Error.item_to_raw
+  ]
+  (** Union possible output errors of the transforms
+      leading to {!Biocaml_sam.item} values.
+      (the other output transforms are error-free).  *)
+
+  type tags = t
+  (** Alias to [Tags.t] for this module. *)
+
+  type t = [
+    | `to_sam_item of
+        (Biocaml_sam.item, (string, sam_output_error) Core.Result.t)
+          Biocaml_transform.t
+    | `to_gff of(Biocaml_gff.item, string) Biocaml_transform.t
+    | `to_wig of (Biocaml_wig.item, string) Biocaml_transform.t
+    | `to_bed of (Biocaml_bed.item, string) Biocaml_transform.t
+    | `to_fastq of (Biocaml_fastq.item, string) Biocaml_transform.t
+    | `to_char_fasta of
+        (Biocaml_fasta.char_seq Biocaml_fasta.raw_item, string)
+          Biocaml_transform.t
+    | `to_int_fasta of
+        (Biocaml_fasta.int_seq Biocaml_fasta.raw_item, string)
+          Biocaml_transform.t
+    | `to_table of (Biocaml_table.Row.t, string) Biocaml_transform.t
+  ]
+  (** Generic union of possible output transforms. *)
+
+  val name: t -> string
+  (** Get a string naming the transform (for debug/error messages). *)
+
+  val from_tags : ?zip_level:int -> ?zlib_buffer_size:int -> tags ->
+    (t, [> `not_implemented of string ]) Biocaml_internal_pervasives.Result.t
+  (** Guess the [Output_transform.t] from file tags. *)
+
+  val sexp_of_sam_output_error: sam_output_error -> Sexplib.Sexp.t
+
+end
+
+
 (** {2 S-Expression Conversions} *)
 
 val file_format_of_sexp: Sexplib.Sexp.t -> file_format
 val sexp_of_file_format: file_format -> Sexplib.Sexp.t
 include Core.Sexpable.S with type t := t
-
