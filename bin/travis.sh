7c4f99d12bea8e2722f977a44503c80082c83fd4
Ashish Agarwal <agarwal1975@gmail.com>
2014-01-27 13:02:07 -0500

    travis.sh: install aspcud
    This is the recommended solver for OPAM, which will use it by default
    if available.

--- bin/travis.sh
+++ bin/travis.sh
@@ -12,7 +12,7 @@ esac
 
 sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra aspcud opam
 export OPAMYES=1
 export OPAMVERBOSE=1
 ocaml -version
