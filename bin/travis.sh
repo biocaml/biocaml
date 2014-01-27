biocaml/biocaml

commit bdccc30301df02d8b802d6a53d2e12f99f5667c2

travish.sh: delete superfluous echo commands
Travis output prints the script’s commands, so the echo commands were
repetitive. Not sure what `echo “yes”` in line 13 accomplished.

bin/travis.sh
@@ -10,14 +10,12 @@ case "$OCAML_VERSION,$OPAM_VERSION" in
 *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
 esac
 
-echo "yes" | sudo add-apt-repository ppa:$ppa
+sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 export OPAMYES=1
 export OPAMVERBOSE=1
-echo OCaml version
 ocaml -version
-echo OPAM versions
 opam --version
 opam --git-version
 
@@ -27,16 +25,10 @@ opam remote add biorepo git://github.com/biocaml/dev-opam-repo.git
 opam update
 opam pin flow git://github.com/smondet/flow
 
-echo "==== Installing $OPAM_DEPENDS ===="
 opam install ${OPAM_DEPENDS}
 
 eval `opam config env`
-echo "==== Build ===="
 omake
-echo "==== Test ===="
 omake run_tests
-echo "==== Bench ===="
 _build/benchmarks/biocaml_benchmarks -help
-echo "==== Doc ===="
 omake doc
-

commit 455bc7505274650cdb3192d3a2dc42103c2216ee

travis.sh: add -y option to add-apt-repository
Without this, the build hangs awaiting a user response. Bug was
introduced in bdccc30301df02d8b802d6a53d2e12f99f5667c2, which deleted
the echo “yes” piped to this command. Using -y seems better.

bin/travis.sh
@@ -10,7 +10,7 @@ case "$OCAML_VERSION,$OPAM_VERSION" in
 *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
 esac
 
-sudo add-apt-repository ppa:$ppa
+sudo add-apt-repository -y ppa:$ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra aspcud opam
 export OPAMYES=1