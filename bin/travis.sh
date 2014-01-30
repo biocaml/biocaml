b3c1b50256d8dfa824f8f3323a86bd97a5567189
Ashish Agarwal <agarwal1975@gmail.com>
2014-01-30 10:23:59 -0500

    travis: add minor comments

--- bin/travis.sh
+++ bin/travis.sh
@@ -1,3 +1,4 @@
+# install Ubuntu packages
 case "$OCAML_VERSION,$OPAM_VERSION" in
 3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
 3.12.1,1.1.0) ppa=avsm/ocaml312+opam11 ;;
@@ -11,15 +12,17 @@ esac
 sudo add-apt-repository -y ppa:$ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra aspcud opam
+
+# configure and view settings
 export OPAMYES=1
 ocaml -version
 opam --version
 opam --git-version
 
+# install OCaml packages
 opam init 
 eval `opam config env`
 
-# install dependencies
 opam install \
   ocamlfind \
   omake \
@@ -38,6 +41,7 @@ opam repo add future-dev ~/future/etc/opam
 opam pin future ~/future
 opam install future
 
+# run tests
 cd $TRAVIS_BUILD_DIR
 omake
 _build/tests/biocaml_tests
