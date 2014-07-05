cb908425b726bd42320ffe8d20078111bf530cdd
pveber <philippe.veber@gmail.com>
2014-07-05 09:07:27 +0200

    genomeMap: improved API and doc

--- src/lib/biocaml_genomeMap.ml
+++ src/lib/biocaml_genomeMap.ml
@@ -2,117 +2,132 @@ open Core.Std
 open Biocaml_internal_utils
 open Stream.Infix
 
-type range = Biocaml_range.t
-type 'a location = 'a * range
-
-module Range = Biocaml_range
-module Accu = Biocaml_accu
-
-module Map = struct
-  include Map.Poly.Tree
-
-  let to_stream t = Stream.of_list (to_alist t)
-  let of_stream xs =
-    Stream.fold xs ~init:empty ~f:(fun accu (key,data) -> add accu ~key ~data)
+module type Chromosome = sig
+  type t
+  val compare : t -> t -> int
 end
 
-module Selection = struct
-  type 'a t = ('a, Biocaml_iset.t) Map.t
+module Make(Chromosome : Chromosome) = struct
+  type range = Biocaml_range.t
+  type location = Chromosome.t * range
 
-  let inter u v =
-    Map.fold u ~init:Map.empty ~f:(fun ~key:k ~data:set_u accu ->
-      match Map.find v k with
-      | Some set_v -> Map.add accu ~key:k ~data:(Biocaml_iset.inter set_u set_v)
-      | None -> accu
-    )
+  module Range = Biocaml_range
+  module Accu = Biocaml_accu
 
-  let union u v =
-    let keys = List.dedup (Map.keys u @ Map.keys v) in
-    List.fold keys ~init:Map.empty ~f:(fun accu k ->
-      Map.add accu ~key:k ~data:(
-        Biocaml_iset.union
-          (Option.value (Map.find u k) ~default:Biocaml_iset.empty)
-          (Option.value (Map.find v k) ~default:Biocaml_iset.empty)
+  module Map = struct
+    include Map.Make(struct
+        include Chromosome
+        let sexp_of_t _ = assert false
+        let t_of_sexp _ = assert false
+      end)
+
+    let to_stream t = Stream.of_list (to_alist t)
+    let of_stream xs =
+      Stream.fold xs ~init:empty ~f:(fun accu (key,data) -> add accu key data)
+  end
+
+  module Selection = struct
+    type t = Biocaml_iset.t Map.t
+
+    let inter u v =
+      Map.fold u ~init:Map.empty ~f:(fun ~key:k ~data:set_u accu ->
+          match Map.find v k with
+          | Some set_v -> Map.add accu ~key:k ~data:(Biocaml_iset.inter set_u set_v)
+          | None -> accu
+        )
+
+    let union u v =
+      let keys = List.dedup (Map.keys u @ Map.keys v) in
+      List.fold keys ~init:Map.empty ~f:(fun accu k ->
+          Map.add accu ~key:k ~data:(
+            Biocaml_iset.union
+              (Option.value (Map.find u k) ~default:Biocaml_iset.empty)
+              (Option.value (Map.find v k) ~default:Biocaml_iset.empty)
+          )
+        )
+
+    let diff u v =
+      Map.fold u ~init:Map.empty ~f:(fun ~key:k ~data:set_u accu ->
+          let set_u' =
+            match Map.find v k with
+            | Some set_v -> Biocaml_iset.diff set_u set_v
+            | None -> set_u
+          in
+          Map.add ~key:k ~data:set_u' accu
+        )
+
+    let size x =
+      Map.fold x ~init:0 ~f:(fun ~key ~data:set accu -> Biocaml_iset.cardinal set + accu)
+
+    let overlap sel (k,r) = Biocaml_iset.(
+        match Map.find sel k with
+        | Some x ->
+          inter Range.(add_range empty r.lo r.hi) x
+          |> cardinal
+        | None -> 0
       )
-    )
 
-  let diff u v =
-    Map.fold u ~init:Map.empty ~f:(fun ~key:k ~data:set_u accu ->
-      let set_u' =
-	match Map.find v k with
-	| Some set_v -> Biocaml_iset.diff set_u set_v
-	| None -> set_u
-      in 
-      Map.add ~key:k ~data:set_u' accu
-    )
+    let intersects sel (k,r) =
+      Option.value_map
+        (Map.find sel k)
+        ~default:false
+        ~f:(fun x -> Range.(Biocaml_iset.intersects_range x r.lo r.hi))
 
-  let size x =
-    Map.fold x ~init:0 ~f:(fun ~key ~data:set accu -> Biocaml_iset.cardinal set + accu)
-
-  let intersection_size (k,r) dom = Biocaml_iset.(
-    match Map.find dom k with
-    | Some x -> 
-      inter Range.(add_range empty r.lo r.hi) x
-      |> cardinal
-    | None -> 0
-  )
-
-  let intersects (k,r) dom =
-    Option.value_map
-      (Map.find dom k)
-      ~default:false
-      ~f:(fun x -> Range.(Biocaml_iset.intersects_range x r.lo r.hi))
-      
-  let to_stream dom =
-    (Map.to_stream dom) 
-    /@ (fun (k,s) -> Stream.map ~f:(fun (lo,hi) -> k, ok_exn (Range.make lo hi)) (Biocaml_iset.to_stream s))
+    let to_stream sel =
+      (Map.to_stream sel)
+      /@ (fun (k,s) -> Stream.map ~f:(fun (lo,hi) -> k, ok_exn (Range.make lo hi)) (Biocaml_iset.to_stream s))
       |> Stream.concat
 
-  let of_stream e =
-    let accu = Accu.create Biocaml_iset.empty fst (fun (_,r) -> Range.(fun x -> Biocaml_iset.add_range x r.lo r.hi)) in
-    Stream.iter ~f:(fun loc -> Accu.add accu loc loc ) e ;
-    Map.of_stream (Accu.stream accu)
-end
+    let of_stream e =
+      let accu = Accu.create Biocaml_iset.empty fst (fun (_,r) -> Range.(fun x -> Biocaml_iset.add_range x r.lo r.hi)) in
+      Stream.iter ~f:(fun loc -> Accu.add accu loc loc ) e ;
+      Map.of_stream (Accu.stream accu)
+  end
 
 
-module type Signal = sig
-  type ('a,'b) t
-  val make : ('b list -> 'c) -> ('a location * 'c) Stream.t -> ('a,'c) t
+  module type Signal = sig
+    type 'a t
 
-  val eval : 'a -> int -> ('a,'b) t -> 'b
+    val eval : 'a t -> default:'a -> Chromosome.t -> int -> 'a
+    (** function evaluation at some point in the genome *)
 
-  val fold : ('a -> Range.t -> 'b -> 'c -> 'c) -> ('a,'b) t -> 'c -> 'c
+    val fold : 'a t -> init:'c -> f:('c -> location -> 'b -> 'c) -> 'c
+    (** folds on constant intervals of the function, in increasing order *)
 
-  val to_stream : ('a,'b) t -> ('a location * 'b) Stream.t
-end
+    val to_stream : 'a t -> (location * 'a) Stream.t
+    (** enumeration over all constant intervals of the function, in
+        increasing order *)
 
-module LMap = struct
-  module T = Biocaml_interval_tree
+    val of_stream : ('a -> 'a -> 'a) -> (location * 'a) Stream.t -> 'a t
+  end
 
-  type ('a,'b) t = ('a, 'b T.t) Map.t
+  module LMap = struct
+    module T = Biocaml_interval_tree
 
-  let intersects (k,r) lmap =
+    type 'a t = 'a T.t Map.t
+
+  let intersects lmap (k,r) =
     Option.value_map (Map.find lmap k) ~default:false ~f:(fun x -> Range.(T.intersects x r.lo r.hi))
 
-  let closest (k,r) lmap = 
+  let closest lmap (k,r) =
     Option.bind
       (Map.find lmap k)
       Range.(fun x ->
-	try
-	  let lo,hi,label,d = T.find_closest r.lo r.hi x in
-	  Some ((k, ok_exn (make lo hi)), label, d)
-	with T.Empty_tree -> None
-      )
+          try
+            let lo,hi,label,d = T.find_closest r.lo r.hi x in
+            Some ((k, ok_exn (make lo hi)), label, d)
+          with T.Empty_tree -> None
+        )
 
-  let intersecting_elems (k, { Range.lo ; hi }) lmap =
+  let intersecting_elems lmap (k, { Range.lo ; hi }) =
     match Map.find lmap k with
     | Some x ->
       T.find_intersecting_elem lo hi x
       /@ (fun (lo,hi,x) -> (k, ok_exn (Range.make lo hi)), x)
     | None -> Stream.empty ()
 
-  let to_stream dom =
-    (Map.to_stream dom) 
+  let to_stream lmap =
+    (Map.to_stream lmap)
     /@ (fun (k,t) -> Stream.map ~f:(fun (lo,hi,x) -> (k, ok_exn (Range.make lo hi)), x) (T.to_stream t))
     |> Stream.concat
 
@@ -123,41 +138,24 @@ module LMap = struct
     Stream.iter ~f:(fun loc -> Accu.add accu loc loc ) e ;
     Map.of_stream (Accu.stream accu)
 
+  end
+
+  module LSet = struct
+    module T = Biocaml_interval_tree
+
+    type t = unit T.t Map.t
+
+    let intersects = LMap.intersects
+
+    let closest lset loc =
+      Option.map (LMap.closest lset loc) ~f:(fun (loc', (), d) -> loc', d)
+
+    let intersecting_elems lset loc =
+      LMap.intersecting_elems lset loc /@ fst
+
+    let to_stream lset = LMap.to_stream lset /@ fst
+    let of_stream e = e /@ (fun x -> x, ()) |> LMap.of_stream
+
+  end
+
 end
-
-module LSet = struct
-  module T = Biocaml_interval_tree
-
-  type 'a t = ('a, unit T.t) Map.t
-
-  let intersects = LMap.intersects
-
-  let closest loc lset =
-    Option.map (LMap.closest loc lset) ~f:(fun (loc', (), d) -> loc', d)
-
-  let intersecting_elems loc lset = 
-    LMap.intersecting_elems loc lset /@ fst
-
-  let to_stream lset = LMap.to_stream lset /@ fst
-  let of_stream e = e /@ (fun x -> x, ()) |> LMap.of_stream
-
-end
-
-
-module type LMap_spec = sig
-  type ('a,'b) t
-
-  val make : ('a location * 'b) Stream.t -> ('a,'b) t
-
-  val fold : ('a -> Range.t -> 'b -> 'c -> 'c) -> ('a,'b) t -> 'c -> 'c
-
-  val pwfold : ('a -> Range.t -> 'b list -> 'c -> 'c) -> ('a, 'b) t -> 'c -> 'c
-
-  val intersects : 'a location -> ('a,'b) t -> bool
-
-  val to_stream : ('a,'b) t -> ('a location * 'b) Stream.t
-
-  val union : ('a,'b) t -> ('a,'b) t -> ('a,'b) t
-  val add : 'a location -> 'b -> ('a,'b) t -> ('a,'b) t
-end
-
--- src/lib/biocaml_genomeMap.mli
+++ src/lib/biocaml_genomeMap.mli
@@ -1,120 +1,135 @@
-open Core.Std
+(**
+   Data structures to represent sets of (possibly annotated) genomic regions
 
-type range = Biocaml_range.t
-type 'a location = 'a * range
+   This module is useful to deal with sets of genomic regions. It
+   provides set operations like union, intersection, difference or
+   membership tests. Specific data types are also provided when the
+   regions are annotated with some value.
 
-(** Indicator function on a genome *)
-module Selection : sig
-  type 'a t
+   Genomic regions are represented as a pair formed by a range and an
+   abstract representation of a sequence/chromosome identifiers. The
+   data structures implemented here are parameterized over this
+   abstract type, but the module provides an implementation for the
+   most common case where chromosomes are identified with a string.
 
-  val inter : 'a t -> 'a t -> 'a t
-  val union : 'a t -> 'a t -> 'a t
-  val diff : 'a t -> 'a t -> 'a t
-  val size : 'a t -> int
+   The functor [Make] provides four datatypes, which corresponds to
+   variants where:
 
-  val intersects : 'a location -> 'a t -> bool
+   - the regions in the set can overlap or not
+
+   - the regions are annotated with some values
+*)
+
+(** An abstract representation of a chromosome identifier *)
+module type Chromosome = sig
+  type t
+  val compare : t -> t -> int
+end
+
+module Make(Chromosome : Chromosome) : sig
+  type range = Biocaml_range.t
+  type location = Chromosome.t * range
+
+  (** A collection of non-overlapping regions *)
+  module Selection : sig
+    type t
+
+    val inter : t -> t -> t
+    val union : t -> t -> t
+    val diff : t -> t -> t
+    val size : t -> int
+
+    val intersects : t -> location -> bool
     (** [intersects loc sel] returns [true] if [loc] has a non-empty
         intersection with [sel], and [false] otherwise. *)
 
-  val intersection_size : 'a location -> 'a t -> int
+    val overlap : t -> location -> int
 
-  val to_stream : 'a t -> 'a location Stream.t
+    val to_stream : t -> location Stream.t
 
-  val of_stream : 'a location Stream.t -> 'a t
-    (** [of_stream e] computes a selection as the union of the locations contained in [e] *)
+    val of_stream : location Stream.t -> t
+    (** [of_stream e] computes a selection (i.e. a set of non
+        overlapping locations) as the union of the locations contained
+        in [e] *)
 
-end
+  end
 
-(** Partial function over the genome: each base may be associated to a value. *)
-module type Signal = sig
-  type ('a,'b) t
-  val make : ('b list -> 'c) -> ('a location * 'c) Stream.t -> ('a,'c) t
+  (** Partial function over the genome
 
-  val eval : 'a -> int -> ('a,'b) t -> 'b
-  (** function evaluation at some point in the genome *)
+      This module implements a partial function over the genome: each
+      base may be associated to a value. Alternatively, this can be
+      seen as a collection of non-overlapping regions each labeled
+      with a value *)
+  module type Signal = sig
+    type 'a t
 
-  val fold : ('a -> range -> 'b -> 'c -> 'c) -> ('a,'b) t -> 'c -> 'c
-  (** folds on constant intervals of the function, in increasing order *)
+    val eval : 'a t -> default:'a -> Chromosome.t -> int -> 'a
+    (** function evaluation at some point in the genome *)
 
-  val to_stream : ('a,'b) t -> ('a location * 'b) Stream.t
-  (** enumeration over all constant intervals of the function, in increasing order *)
-end
+    val fold : 'a t -> init:'c -> f:('c -> location -> 'b -> 'c) -> 'c
+    (** folds on constant intervals of the function, in increasing order *)
 
-(** A set of locations *)
-module LSet : sig
-  type 'a t
+    val to_stream : 'a t -> (location * 'a) Stream.t
+    (** stream over all constant intervals of the function, in
+        increasing order *)
 
-  val to_stream : 'a t -> 'a location Stream.t
-  val of_stream : 'a location Stream.t -> 'a t
+    val of_stream : ('a -> 'a -> 'a) -> (location * 'a) Stream.t -> 'a t
+    (** [of_stream f ls] builds a signal from a collection of
+    annotated locations. [f] is used when two locations intersect, to
+    compute the annotation on their intersection. *Beware*, [f]
+    should be associative and commutative since when many locations
+    in [ls] intersect, there is no guarantee on the order followed to
+    aggregate them and their annotation. *)
+
+  end
+
+  (** A set of locations *)
+  module LSet : sig
+    type t
+
+    val to_stream : t -> location Stream.t
+    val of_stream : location Stream.t -> t
 
 
-  (* val fold : ('a -> range -> 'b -> 'b) -> 'a t -> 'b -> 'b *)
-  (*   (\** fold guaranteed on increasing order keywise, and for each key *\) *)
+    val intersects : t -> location -> bool
+    (** [intersects lset loc] returns [true] if [loc] has a non-empty
+        intersection with one of the locations in [lset], and returns
+        [false] otherwise *)
 
-  val intersects : 'a location -> 'a t -> bool
-  (** [intersects loc lmap] returns [true] if [loc] has a non-empty
-      intersection with one of the locations in [lmap], and returns
-      [false] otherwise *)
+    val closest : t -> location -> (location * int) option
+    (** [closest lset loc] returns the location in [lset] that is the
+        closest to [loc], along with the actual (minimal)
+        distance. Returns [None] if there is no location in [lset]
+        that comes from the same chromosome than [loc]. *)
 
-  val closest : 'a location -> 'a t -> ('a location * int) option
-  (** [closest loc lset] returns the location in [lset] that is the
-      closest to [loc], along with the actual (minimal)
-      distance. Returns [None] if there is no location in [lset]
-      that comes from the same sequence than [loc]. *)
-    
-  val intersecting_elems : 'a location -> 'a t -> 'a location Stream.t
-  (** [intersecting_elems loc lset] returns an enumeration of all
-      locations in [lset] that intersect [loc]. *)
+    val intersecting_elems : t -> location -> location Stream.t
+    (** [intersecting_elems lset loc] returns a stream of all
+        locations in [lset] that intersect [loc]. *)
 
-end
+  end
 
-(** A set of locations with an attached value on each of them *)
-module LMap : sig
-  type ('a,'b) t
+  (** A set of locations with an attached value on each of them *)
+  module LMap : sig
+    type 'a t
 
-  val to_stream : ('a, 'b) t -> ('a location * 'b) Stream.t
-  val of_stream : ('a location * 'b) Stream.t -> ('a, 'b) t
+    val to_stream : 'a t -> (location * 'a) Stream.t
+    val of_stream : (location * 'a) Stream.t -> 'a t
 
-  val intersects : 'a location -> ('a,'b) t -> bool
-    (** [intersects loc lmap] returns [true] if [loc] has a non-empty
+    val intersects : 'a t -> location -> bool
+    (** [intersects lmap loc] returns [true] if [loc] has a non-empty
         intersection with one of the locations in [lmap], and returns
         [false] otherwise *)
 
-  val closest : 'a location -> ('a,'b) t -> ('a location * 'b * int) option
-    (** [closest loc lmap] returns the location in [lmap] that is the 
-        closest to [loc], along with its annotation and the actual (minimal) 
-        distance. Returns [None] if there is no location in [lmap] 
-        that comes from the same sequence than [loc]. *)
+    val closest : 'a t -> location -> (location * 'a * int) option
+    (** [closest lmap loc] returns the location in [lmap] that is the
+        closest to [loc], along with its annotation and the actual (minimal)
+        distance. Returns [None] if there is no location in [lmap]
+        that comes from the same chromosome than [loc]. *)
 
-  val intersecting_elems : 'a location -> ('a, 'b) t -> ('a location * 'b) Stream.t
-  (** [intersecting_elems loc lmap] returns an enumeration of elements
-      in [lmap] whose location intersects with [loc]. *)
+    val intersecting_elems : 'a t -> location -> (location * 'a) Stream.t
+    (** [intersecting_elems lmap loc] returns a stream of elements
+        in [lmap] whose location intersects with [loc]. *)
 
-end
-
-
-(** A set of locations with an attached value on each of them *)
-module type LMap_spec = sig
-  type ('a,'b) t
-
-  val make : ('a location * 'b) Stream.t -> ('a,'b) t
-
-
-  val fold : ('a -> range -> 'b -> 'c -> 'c) -> ('a,'b) t -> 'c -> 'c
-    (** fold guaranteed on increasing order keywise, and for each key *)
-
-  val pwfold : ('a -> range -> 'b list -> 'c -> 'c) -> ('a, 'b) t -> 'c -> 'c
-    (** a constant interval of an LMap [lm] is a range intersecting [lm] and
-that do not contain any start or end of a range from [lm]. A maximal
-constant interval is a constant interval, maximum for inclusion.
-[pwfold f lm init] folds on maximal constant intervals of [lm],
-in increasing order *)
-
-  val intersects : 'a location -> ('a,'b) t -> bool
-
-  val to_stream : ('a,'b) t -> ('a location * 'b) Stream.t
-
-  val union : ('a,'b) t -> ('a,'b) t -> ('a,'b) t
-  val add : 'a location -> 'b -> ('a,'b) t -> ('a,'b) t
+  end
 end
 
