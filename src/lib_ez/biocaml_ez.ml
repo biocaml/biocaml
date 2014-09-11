56ded59c05fcc4e453f9430ad5516d61eb8d67d7
Ashish Agarwal <agarwal1975@gmail.com>
2014-09-11 13:47:39 -0400

    Ez Fastq: add Illumina related functions
    Should have been done with a5f9e260fdbe0cdacef2958c9f788e45c776324f.
    Requires manually defining entire module because can’t modify
    sub-module Illumina after doing include Biocaml_fastq. This causes an
    error saying module Illumina declared twice.

--- src/lib_ez/biocaml_ez.ml
+++ src/lib_ez/biocaml_ez.ml
@@ -2,11 +2,35 @@ open Core.Std
 open Future_std
 
 module Fastq = struct
-  include Biocaml_fastq
+  open Biocaml_fastq
+
+  type nonrec item = item
+  let split_name = split_name
 
   let read ic =
     read ic
     |> Pipe.map ~f:ok_exn
+
+  let write = write
+  let write_file = write_file
+
+  module Illumina = struct
+    open Illumina
+    type nonrec surface = surface
+    type nonrec tile = tile
+    let tile_of_string x = ok_exn (tile_of_string x)
+    let tile_to_string = tile_to_string
+    type nonrec sequence_id = sequence_id
+    let sequence_id_of_string x = ok_exn (sequence_id_of_string x)
+  end
+
+  let item_to_string = item_to_string
+
+  let name_of_line ?pos x = ok_exn (name_of_line ?pos x)
+  let sequence_of_line = sequence_of_line
+  let comment_of_line ?pos x = ok_exn (comment_of_line ?pos x)
+  let qualities_of_line ?pos ?sequence x =
+    ok_exn (qualities_of_line ?pos ?sequence x)
 
 end
 
