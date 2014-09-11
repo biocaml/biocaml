55b4f144f7b86d4d5ae7518b0ee46fb90b1164d5
Ashish Agarwal <agarwal1975@gmail.com>
2014-09-11 11:02:12 -0400

    Fastq: remove deprecated API
    We previously implemented our new style of API, i.e. using Or_error and
    Future. Now deleted old functions and error types.

--- src/lib/biocaml_fastq.ml
+++ src/lib/biocaml_fastq.ml
@@ -10,11 +10,6 @@ type item = {
 } with sexp
 
 
-module Err = Biocaml_fastq_error
-exception Parse_error of Err.parsing
-exception Err of Err.t
-
-
 (******************************************************************************)
 (* Printing                                                                   *)
 (******************************************************************************)
@@ -67,130 +62,8 @@ let qualities_of_line ?(pos=Pos.unknown) ?sequence line =
 
 
 (******************************************************************************)
-(* Transforms                                                                 *)
-(******************************************************************************)
-module Transform = struct
-  let string_to_item ?filename () =
-    let name = sprintf "fastq_parser:%s" Option.(value ~default:"<>" filename) in
-    Lines.Transform.make_merge_error
-      ~name ?filename ~next:(fun p ->
-        let open Lines.Buffer in
-        if queued_lines p < 4 then
-          `not_ready
-        else (
-          let name_line  = (next_line_exn p :> string) in
-          if String.length name_line = 0 || name_line.[0] <> '@'
-          then `output (Error (`invalid_name (current_position p, name_line)))
-          else
-            let sequence     = (next_line_exn p :> string) in
-            let comment_line = (next_line_exn p :> string) in
-            if String.length comment_line = 0 || comment_line.[0] <> '+'
-            then `output (Error (`invalid_comment (current_position p, comment_line)))
-            else
-              let qualities    = (next_line_exn p :> string) in
-              if String.length sequence <> String.length qualities
-              then `output (Error
-                (`sequence_qualities_mismatch (current_position p,
-                                                      sequence, qualities)))
-              else (
-                `output (Ok {
-                  name = String.sub name_line 1 (String.length name_line - 1);
-                  comment = String.sub comment_line 1 (String.length comment_line - 1);
-                  sequence; qualities })
-              ))
-      ) ()
-
-  let item_to_string () =
-    Biocaml_transform.of_function ~name:"fastq_to_string" item_to_string
-
-  let trim (specification: [`beginning of int|`ending of int]) =
-    let items =  Queue.create () in
-    let name =
-      sprintf "(fastq_trimmer %s)"
-        (match specification with
-        | `beginning i -> sprintf "B:%d" i
-        | `ending i -> sprintf "E:%d" i) in
-    Biocaml_transform.make ~name ()
-      ~feed:(fun r -> Queue.enqueue items r)
-      ~next:(fun stopped ->
-        begin match Queue.dequeue items with
-        | Some r ->
-          let rlgth = String.length r.sequence in
-          begin match specification with
-          | `beginning i when i < rlgth ->
-            `output (Ok
-              { r with sequence = String.sub r.sequence ~pos:i ~len:(rlgth - i);
-                qualities = String.sub r.qualities ~pos:i ~len:(rlgth - i) })
-          | `ending i when i < rlgth ->
-            `output (Ok
-              { r with sequence = String.sub r.sequence ~pos:0 ~len:(rlgth - i);
-                qualities = String.sub r.qualities ~pos:0 ~len:(rlgth - i) })
-          | _ ->
-            `output (Error (`invalid_size rlgth))
-          end
-        | None -> if stopped then `end_of_stream else `not_ready
-        end)
-
-
-  let fasta_pair_to_fastq ?(phred_score_offset=`Offset33) () =
-    let open Result.Monad_infix in
-    let module Fasta = Biocaml_fasta in
-    Biocaml_transform.of_function begin fun (char_item, int_item) ->
-      if char_item.Fasta.header = int_item.Fasta.header then
-        begin
-          begin try
-            List.map int_item.Fasta.sequence (fun int ->
-                Biocaml_phred_score.(
-                  ok_exn (of_int int)
-                  |> fun x -> ok_exn (to_char ~offset:phred_score_offset x)
-                  |> Char.to_string))
-            |> String.concat ~sep:"" |> Result.return
-          with _ ->
-            Error (`cannot_convert_to_phred_score int_item.Fasta.sequence)
-          end
-          >>= fun qualities ->
-          Ok {name = char_item.Fasta.header;
-                  sequence = char_item.Fasta.sequence;
-                  comment = char_item.Fasta.header;
-                  qualities}
-        end
-      else
-        Error (`sequence_names_mismatch (char_item.Fasta.header,
-                                        int_item.Fasta.header))
-    end
-
-  let fastq_to_fasta_pair  ?(phred_score_offset=`Offset33) () =
-    Biocaml_transform.of_function begin fun {name; sequence; qualities; _} ->
-      begin try
-        let scores =
-          String.fold ~init:[] qualities ~f:(fun prev c ->
-              Biocaml_phred_score.(
-                of_char ~offset:phred_score_offset c |> ok_exn |> to_int) :: prev)
-          |> List.rev in
-        Ok Biocaml_fasta.({ header = name; sequence },
-                              { header = name; sequence = scores })
-      with e -> (* exception from the Phred-score convertions *)
-        Error (`cannot_convert_ascii_phred_score qualities)
-      end
-    end
-
-end
-
-
-(******************************************************************************)
 (* Input/Output                                                               *)
 (******************************************************************************)
-let in_channel_to_item_stream ?(buffer_size=65536) ?filename inp =
-  Transform.string_to_item ?filename ()
-  |> Biocaml_transform.in_channel_strings_to_stream ~buffer_size inp
-
-let error_to_exn err = Err err
-
-let in_channel_to_item_stream_exn ?(buffer_size=65536) ?filename inp =
-  Stream.result_to_exn ~error_to_exn (
-    in_channel_to_item_stream ~buffer_size ?filename inp
-  )
-
 module MakeIO (Future : Future.S) = struct
   open Future
 
--- src/lib/biocaml_fastq.mli
+++ src/lib/biocaml_fastq.mli
@@ -10,28 +10,9 @@ type item = {
 } with sexp
 
 
-module Err : module type of Biocaml_fastq_error
-exception Parse_error of Err.parsing
-exception Err of Err.t
-
-
+(******************************************************************************)
 (** {2 Input/Output } *)
-
-(** Parse an input-channel into a stream of [item] results. *)
-val in_channel_to_item_stream :
-  ?buffer_size:int ->
-  ?filename:string ->
-  in_channel ->
-  (item, [> Err.parsing]) Result.t Stream.t
-
-(** Returns a stream of [item]s. [Stream.next] will raise [Err _] in
-    case of any error. *)
-val in_channel_to_item_stream_exn :
-  ?buffer_size:int ->
-  ?filename:string ->
-  in_channel ->
-  item Stream.t
-
+(******************************************************************************)
 module MakeIO (Future : Future.S) : sig
   open Future
 
@@ -50,27 +31,18 @@ end
 include module type of MakeIO(Future_std)
 
 
-(** {2 Printing } *)
-
+(******************************************************************************)
+(** {2 Printing} *)
+(******************************************************************************)
 (** This function converts [item] values to strings that can be dumped
     to a file, i.e. they contain full-lines, including {i all}
     end-of-line characters. *)
 val item_to_string: item -> string
 
 
-(** {2 Parsing }
-
-    Parsing functions. Mostly needed only internally. Each function takes:
-
-    - [line] - The line to parse.
-
-    - [pos] - Optional position of the line used in error
-    reporting. The column should always be 1 because by definition a
-    line starts at the beginning.
-
-    Functions ending in _exn raise Parse_error.
-*)
-
+(******************************************************************************)
+(** {2 Parsing} *)
+(******************************************************************************)
 val name_of_line : ?pos:Pos.t -> Line.t -> string Or_error.t
 val sequence_of_line : ?pos:Pos.t -> Line.t -> string
 val comment_of_line : ?pos:Pos.t -> Line.t -> string Or_error.t
@@ -84,50 +56,3 @@ val qualities_of_line :
   ?sequence:string ->
   Line.t ->
   string Or_error.t
-
-
-(** {2 Transforms } *)
-
-module Transform: sig
-  (** Lower-level transforms. *)
-
-  val string_to_item:
-    ?filename:string -> unit ->
-    (string, (item, [> Err.parsing]) Result.t) Biocaml_transform.t
-  (** Create a [Biocaml_transform.t] from arbitrary strings to
-      [item] values.*)
-
-  val item_to_string: unit -> (item, string) Biocaml_transform.t
-  (** Create a [Biocaml_transform.t] from [item] values to strings. *)
-
-  val trim:
-    [ `beginning of int | `ending of int ] ->
-    (item, (item, [> `invalid_size of int]) Result.t) Biocaml_transform.t
-  (** Create a [Biocaml_transform.t] that trims FASTQ items. *)
-
-  val fasta_pair_to_fastq:
-    ?phred_score_offset:Biocaml_phred_score.offset ->
-    unit ->
-    (Biocaml_fasta.char_seq Biocaml_fasta.item *
-       Biocaml_fasta.int_seq Biocaml_fasta.item,
-     (item,
-      [> Err.fasta_pair_to_fastq ]) Result.t)
-      Biocaml_transform.t
-  (** Create a transform that builds [item] records thanks
-      to sequences from [Fasta.(char_seq item)] values
-      and qualities converted from
-      [Fasta.(int_seq item)] values. The default Phred score encoding
-      is [`offset33] (like in {!Biocaml_phred_score}). *)
-
-  val fastq_to_fasta_pair :
-    ?phred_score_offset:Biocaml_phred_score.offset ->
-    unit ->
-    (item,
-     (Biocaml_fasta.char_seq Biocaml_fasta.item *
-        Biocaml_fasta.int_seq Biocaml_fasta.item,
-      [> `cannot_convert_ascii_phred_score of string ]) Result.t)
-      Biocaml_transform.t
-  (** Create a transform that split a FASTQ item into to FASTA items
-      (i.e. the inverse of {!fasta_pair_to_fastq}). *)
-
-end
