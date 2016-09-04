35e5596895db838f08a55411e2ef35348aea4705
Philippe Veber <philippe.veber@gmail.com>
2016-09-04 15:06:57 +0200

    unix/Cel: white space

--- lib/unix/cel.mli
+++ lib/unix/cel.mli
@@ -9,13 +9,13 @@ type t
 
 exception Bad of string
 (** Raised when encountering ill-formed CEL type. *)
-    
+
 val of_file : string -> t
 (** Parse given file if possible. Raise [Bad] if there are any parse errors. *)
-  
+
 val of_file_opt : string -> t option
 (** Parse given file if possible. *)
-  
+
 
 (** {6 Operations on Intensity Section} *)
 
@@ -24,7 +24,7 @@ type idata = {
   stdv:float; (** standard deviation of intensity *)
   npixels:int (** number of pixels used in mean/stdv calculation *)
 }
-    
+
 (** Represents row in intensity section *)
 type irow = {
   xcoord:int; (** x-coordinate *)
@@ -33,26 +33,26 @@ type irow = {
 }
 
 val ifold : ('a -> irow -> 'a) -> 'a -> t -> 'a
-  (** [ifold f a t] folds over data rows in intensity section of [t]. *) 
+  (** [ifold f a t] folds over data rows in intensity section of [t]. *)
 
 val iiter : (irow -> unit) -> t -> unit
   (** [iiter f t] iterates over the rows in intensity section of [t]. *)
-  
+
 val data :
   Bpmap.t -> t list -> (Bpmap.probe * (idata * idata) list) list
 (** [data bpmap cels] returns a list associating probes with pairs of
     (PM,MM) idata in each of the given cel files (in the same order of
     course). Raise [Failure] if any file in [cels] lacks a value for any
     probe in [bpmap]. *)
-  
+
 val pm_mm : Bpmap.t -> t list -> (Bpmap.probe * float list) list
 (** Similar to {!data} but the data returned are the PM-MM mean
     intensity values in [cels]. *)
-  
+
 val pm : Bpmap.t -> t list -> (Bpmap.probe * float list) list
 (** Similar to {!data} but the data returned are the PM mean intensity
     values in [cels]. *)
-  
+
 val mm : Bpmap.t -> t list -> (Bpmap.probe * float list) list
 (** Similar to {!data} but the data returned are the MM mean intensity
     values in [cels]. *)
