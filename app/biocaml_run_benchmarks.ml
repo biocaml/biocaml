4f411e88a0b9902e93ad838f45afeeb623f534a2
Philippe Veber <philippe.veber@gmail.com>
2016-09-23 12:17:36 +0200

    Removed transform related code in benchmark
    The code has been moved to another, dedicated repo: see
    biocaml/streaming-benchmark. With this, the only remaining warnings
    during compilation are related to the use of ppx_compare (see
    janestreet/ppx_compare#1).

--- app/biocaml_run_benchmarks.ml
+++ app/biocaml_run_benchmarks.ml
@@ -6,10 +6,6 @@ let () =
   Command.(
     let whole_thing =
       group ~summary:"Biocaml's benchmarks" [
-        (* ("zip", Benchmark_zip.command); *)
-        (* ("bamsam", Bam_sam_and_the_gc.command); *)
-        ("bamcount", Bam_count_alignments.command);
-        ("samcount", Sam_count_alignments.command);
         ("linesplit", Line_split.command);
       ] in
     run ~version:About.version whole_thing
