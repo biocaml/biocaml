f1a90ac9a28ce4c80fba59efefab2d381dd3dce5
Philippe Veber <philippe.veber@gmail.com>
2017-05-08 22:51:29 +0200

    whitespace commit

--- lib/unix/accu.mli
+++ lib/unix/accu.mli
@@ -1,7 +1,7 @@
-(** 
+(**
     A datastructure (based on Hashtbl) to accumulate values.
 
-    An [Accu.t] can be seen as a generalized histogram: values 
+    An [Accu.t] can be seen as a generalized histogram: values
     are mapped to bins, and each bin has a corresponding value
     which may be its size or its contents depending on the need.
 *)
@@ -10,14 +10,14 @@ open Core_kernel.Std
 (** {7 Generic API} *)
 
 type ('instance,'bin,'increment,'accu) t
-(** General type for accumulators: ['instance]s are mapped to ['bin]s, 
-    and the ['accu]mulated value for a ['bin] is updated with an 
+(** General type for accumulators: ['instance]s are mapped to ['bin]s,
+    and the ['accu]mulated value for a ['bin] is updated with an
     ['increment] *)
 
 val create : ?n:int -> 'd -> ('a -> 'b) -> ('c -> 'd -> 'd) -> ('a,'b,'c,'d) t
 (** [create ~n e f op] creates an accumulator, which maps instances to bins
-    with [f], uses [e] as a neutral element (that is the value associated to a 
-    bin before any value has been added to it) and updates the value of a bin 
+    with [f], uses [e] as a neutral element (that is the value associated to a
+    bin before any value has been added to it) and updates the value of a bin
     with [op]. [n] is an estimation of the maximum number of bins. *)
 
 val add : ('a,'b,'c,'d) t -> 'a -> 'c -> unit
@@ -45,11 +45,11 @@ module Counter : sig
 end
 
 val counts  : ('a -> 'b) -> 'a Stream.t -> ('b * int) Stream.t
-val product : 
-  ?filter:('a -> 'b -> bool) -> 
-  ('a -> 'b -> 'c) -> 
-  'a list -> 'b list -> 
-  ('c * int) Stream.t 
+val product :
+  ?filter:('a -> 'b -> bool) ->
+  ('a -> 'b -> 'c) ->
+  'a list -> 'b list ->
+  ('c * int) Stream.t
 (** [product filter f l1 l2] computes an histogram of values returned by f
     when it is applied for all combinations of elements in [l1] and
     [l2] such that the predicate [filter] is true *)
