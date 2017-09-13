c58671fbb278d62230a0b27c70f7a2bfff48d5eb
Philippe Veber <philippe.veber@gmail.com>
2017-09-13 11:23:44 +0200

    test/Lines: test for base parser

--- lib/test/lines.ml
+++ lib/test/lines.ml
@@ -1,15 +1,36 @@
 open Core_kernel
 open CFStream
-module Line = Biocaml_unix.Line
-module Lines = Biocaml_unix.Lines
 open OUnit
 
-let test_of_string () =
+let check_int = assert_equal ~printer:Int.to_string
+
+let check_string_list =
   assert_equal
     ~printer:(fun x -> Sexp.to_string ([%sexp_of:string list] x))
-    ["" ; "aa" ; "" ; "" ; "bb"]
-    (Lines.of_string "\naa\n\n\nbb" |> Stream.to_list |> List.map ~f:Line.to_string)
+
+let test_base_parser () =
+  let open Biocaml_base in
+  let module P = Lines.Parser in
+  check_int 0 P.(line_number initial_state) ;
+  let s1, l1 = P.(step initial_state) (Some "\naa\n\n\nbb") in
+  check_int 5 (P.line_number s1) ;
+  check_string_list ["" ; "aa" ; "" ; "" ] (l1 :> string list) ;
+  let s2, l2 = P.step s1 (Some "aa\n\n\n") in
+  check_int 7 (P.line_number s2) ;
+  check_string_list ["bbaa" ; "" ; ""] (l2 :> string list)
+
+module Line = Biocaml_unix.Line
+module Lines = Biocaml_unix.Lines
+
+let test_of_string () =
+  let f input answer =
+    check_string_list
+      answer
+      (Lines.of_string input |> Stream.to_list |> List.map ~f:Line.to_string)
+  in
+  f "\naa\n\n\nbb" ["" ; "aa" ; "" ; "" ; "bb"]
 
 let tests = "Lines" >::: [
-  "of_string" >:: test_of_string
-]
+    "Base.Lines.Parser" >:: test_base_parser ;
+    "of_string" >:: test_of_string ;
+  ]
