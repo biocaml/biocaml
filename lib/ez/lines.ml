c81e267b591dbda002ca72b99aea1d6569347055
Philippe Veber <philippe.veber@gmail.com>
2017-09-15 00:12:56 +0200

    ez/Lines: add file mapper

---
+++ lib/ez/lines.ml
@@ -0,0 +1,12 @@
+open CFStream
+open Core_kernel
+include Biocaml_unix.Lines
+
+let file_mapper inbed outbed ~f =
+  In_channel.with_file inbed ~f:(fun ic ->
+      Out_channel.with_file outbed ~f:(fun oc ->
+          read ic
+          |> Stream.concat_map ~f:(fun x -> Stream.of_list (f x))
+          |> write oc
+        )
+    )
---
+++ lib/ez/lines.mli
@@ -0,0 +1,7 @@
+include module type of Biocaml_unix.Lines
+
+val file_mapper :
+  string ->
+  string ->
+  f:(item -> item list) ->
+  unit
