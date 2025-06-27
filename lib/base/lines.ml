dbe3ee220ba26bcd0d9156aa35f52bb821c75569
Ashish Agarwal <ashish@solvuu.com>
2025-06-27 16:54:38 -0500

    Port Lines tests

--- lib/base/lines.ml
+++ lib/base/lines.ml
@@ -32,3 +32,18 @@ module Parser = struct
           , Line.append value h :: t )))
   ;;
 end
+
+module Test = struct
+  let%test _ =
+    let s1, l1 = Parser.(step initial_state) (Some "\naa\n\n\nbb") in
+    let s2, l2 = Parser.step s1 (Some "aa\n\n\n") in
+    let s3, l3 = Parser.step s2 None in
+    Int.equal 0 Parser.(line_number initial_state)
+    && Int.equal 5 (Parser.line_number s1)
+    && List.equal String.equal [ ""; "aa"; ""; "" ] (l1 :> string list)
+    && Int.equal 7 (Parser.line_number s2)
+    && List.equal String.equal [ "bbaa"; ""; "" ] (l2 :> string list)
+    && Int.equal 7 (Parser.line_number s3)
+    && List.equal String.equal [] (l3 :> string list)
+  ;;
+end
