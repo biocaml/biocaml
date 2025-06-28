a2ffb76e6ee53704f892ecbb0138d3e81c6c91a4
Ashish Agarwal <ashish@solvuu.com>
2025-06-28 12:07:25 -0500

    Delete unused Utils.Printer

--- lib/test/utils.ml
+++ lib/test/utils.ml
@@ -12,20 +12,3 @@ let with_temp_file pre suff ~f =
   | `Ok y -> y
   | `Error e -> raise e
 ;;
-
-module Printer = struct
-  let string = Fun.id
-  let int = string_of_int
-
-  let option f x =
-    match x with
-    | None -> "None"
-    | Some y -> sprintf "Some (%s)" (f y)
-  ;;
-
-  let or_error f x =
-    match x with
-    | Ok y -> sprintf "Ok (%s)" (f y)
-    | Error e -> sprintf "Error (%s)" (Error.to_string_hum e)
-  ;;
-end
