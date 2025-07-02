8dbbedc4f6000eb59115b7764db3bae89c1ecde0
Ashish Agarwal <ashish@solvuu.com>
2025-07-02 15:42:43 -0500

    Delete unused CFStream functions

--- lib/unix/CFStream.ml
+++ lib/unix/CFStream.ml
@@ -48,59 +48,6 @@ module Stream = struct
 
   let iter xs ~f = iteri xs ~f:(const f)
 
-  let rec iter2i_exn xs ys ~f =
-    match peek xs, peek ys with
-    | Some x, Some y ->
-      f (count xs) (count ys) x y;
-      junk xs;
-      junk ys;
-      iter2i_exn xs ys ~f
-    | None, None -> ()
-    | _, _ -> raise Expected_streams_of_equal_length
-  ;;
-
-  let iter2_exn xs ys ~f = iter2i_exn xs ys ~f:(const (const f))
-
-  let iter2i a b ~f =
-    try iter2i_exn a b ~f with
-    | Expected_streams_of_equal_length -> ()
-  ;;
-
-  let iter2 a b ~f =
-    try iter2_exn a b ~f with
-    | Expected_streams_of_equal_length -> ()
-  ;;
-
-  let rec find_map xs ~f =
-    match next xs with
-    | Some x -> (
-      match f x with
-      | Some _ as y -> y
-      | None -> find_map xs ~f)
-    | None -> None
-  ;;
-
-  let find xs ~f = find_map xs ~f:(fun x -> if f x then Some x else None)
-
-  let find_exn xs ~f =
-    match find xs ~f with
-    | Some x -> x
-    | None -> raise Caml.Not_found
-  ;;
-
-  let exists xs ~f =
-    match find xs ~f with
-    | Some _ -> true
-    | None -> false
-  ;;
-
-  let rec for_all xs ~f =
-    match next xs with
-    | Some x when not (f x) -> false
-    | Some _ -> for_all xs ~f
-    | None -> true
-  ;;
-
   let rec foldi xs ~init ~f =
     match next xs with
     | None -> init
@@ -111,58 +58,6 @@ module Stream = struct
      stream count by one *)
 
   let fold xs ~init ~f = foldi xs ~init ~f:(const f)
-
-  let reduce xs ~f =
-    match next xs with
-    | Some init -> fold xs ~init ~f
-    | None -> invalid_arg "Stream.reduce: stream should contain at least one element"
-  ;;
-
-  let sum = reduce ~f:( + )
-  let fsum = reduce ~f:( +. )
-
-  let rec fold2i_exn xs ys ~init ~f =
-    match next xs, next ys with
-    | Some x, Some y ->
-      let init = f (count xs - 1) (count ys - 1) init x y in
-      (* decrease by one because of the calls to [next] *)
-      fold2i_exn xs ys ~init ~f
-    | None, None -> init
-    | _ -> raise Expected_streams_of_equal_length
-  ;;
-
-  let fold2_exn xs ys ~init ~f = fold2i_exn xs ys ~init ~f:(const (const f))
-
-  let rec fold2i xs ys ~init ~f =
-    match next xs, next ys with
-    | Some x, Some y ->
-      let init = f (count xs - 1) (count ys - 1) init x y in
-      fold2i xs ys ~init ~f
-    | _ -> init
-  ;;
-
-  let fold2 xs ys ~init ~f = fold2i xs ys ~init ~f:(const (const f))
-
-  let scanl xs ~init ~f =
-    let current = ref init in
-    let f i =
-      if i = 0
-      then Some init
-      else (
-        match next xs with
-        | Some x ->
-          current := f !current x;
-          Some !current
-        | None -> None)
-    in
-    from f
-  ;;
-
-  let scan xs ~f =
-    match next xs with
-    | Some init -> scanl xs ~init ~f
-    | None -> empty ()
-  ;;
 
   let take_whilei xs ~f =
     let aux i =
@@ -176,7 +71,6 @@ module Stream = struct
   ;;
 
   let take_while xs ~f = take_whilei xs ~f:(const f)
-  let take xs ~n = take_whilei xs ~f:(fun j _ -> j < n)
 
   let rec drop_whilei xs ~f =
     match peek xs with
@@ -198,141 +92,9 @@ module Stream = struct
       else false)
   ;;
 
-  let skip_whilei xs ~f =
-    drop_whilei xs ~f;
-    xs
-  ;;
-
-  let skip_while xs ~f = skip_whilei xs ~f:(const f)
-
   let skip xs ~n =
     drop xs ~n;
     xs
-  ;;
-
-  let span xs ~f =
-    (*Two possibilities: either the tail has been read
-      already -- in which case all head data has been
-      copied onto the queue -- or the tail hasn't been
-      read -- in which case, stuff should be read from
-      [xs] *)
-    let queue = Queue.create ()
-    and read_from_queue = ref false in
-    let head _ =
-      if !read_from_queue
-      then
-        (* Everything from the head has been copied *)
-        Queue.dequeue queue
-        (* to the queue already                     *)
-      else (
-        match peek xs with
-        | Some x as e when f x ->
-          junk xs;
-          e
-        | _ -> None)
-    and tail _ =
-      if not !read_from_queue
-      then (
-        (*Copy everything to the queue         *)
-        read_from_queue := true;
-        let rec aux () =
-          match peek xs with
-          | Some x when f x ->
-            Queue.enqueue queue x;
-            aux ()
-          | e -> e
-        in
-        aux ())
-      else next xs
-    in
-    from head, from tail
-  ;;
-
-  let group_aux xs map eq =
-    let prev_group_force = ref ignore in
-    let for_each_group _ =
-      !prev_group_force ();
-      match next xs with
-      | None -> None
-      | Some x ->
-        let queue = Queue.create ()
-        and forced = ref false
-        and mapped_x = map x in
-        let aux i =
-          if i = 0
-          then Some x
-          else if !forced
-          then Queue.dequeue queue
-          else (
-            match peek xs with
-            | Some y as e when eq (map y) mapped_x ->
-              junk xs;
-              e
-            | _ -> None)
-        in
-        let force () =
-          forced := true;
-          let rec loop () =
-            match peek xs with
-            | Some y when eq (map y) mapped_x ->
-              junk xs;
-              Queue.enqueue queue y;
-              loop ()
-            | _ -> ()
-          in
-          loop ()
-        in
-        prev_group_force := force;
-        Some (from aux)
-    in
-    from for_each_group
-  ;;
-
-  let group xs ~f = group_aux xs f Poly.( = )
-  let group_by xs ~eq = group_aux xs Fn.id eq
-
-  let chunk2 xs =
-    from (fun _ ->
-      match next xs with
-      | None -> None
-      | Some a -> (
-        match next xs with
-        | None -> raise Premature_end_of_input
-        | Some b -> Some (a, b)))
-  ;;
-
-  let chunk3 xs =
-    from (fun _ ->
-      match next xs with
-      | None -> None
-      | Some a -> (
-        match next xs with
-        | None -> raise Premature_end_of_input
-        | Some b -> (
-          match next xs with
-          | None -> raise Premature_end_of_input
-          | Some c -> Some (a, b, c))))
-  ;;
-
-  let chunk4 xs =
-    from (fun _ ->
-      match next xs with
-      | None -> None
-      | Some a -> (
-        match next xs with
-        | None -> raise Premature_end_of_input
-        | Some b -> (
-          match next xs with
-          | None -> raise Premature_end_of_input
-          | Some c -> (
-            match next xs with
-            | None -> raise Premature_end_of_input
-            | Some d -> Some (a, b, c, d)))))
-  ;;
-
-  let mapi xs ~f =
-    let aux i = Option.map (next xs) ~f:(f i) in
-    from aux
   ;;
 
   let map xs ~f =
@@ -361,18 +123,6 @@ module Stream = struct
       match next xs with
       | Some x when not (f x) -> aux i
       | x -> x
-    in
-    from aux
-  ;;
-
-  let filter_map xs ~f =
-    let rec aux i =
-      match next xs with
-      | Some x -> (
-        match f x with
-        | None -> aux i
-        | x -> x)
-      | None -> None
     in
     from aux
   ;;
@@ -431,120 +181,6 @@ module Stream = struct
     from aux
   ;;
 
-  let combine (xs, ys) =
-    let aux _ =
-      match peek xs, peek ys with
-      | Some x, Some y ->
-        junk xs;
-        junk ys;
-        Some (x, y)
-      | _ -> None
-    in
-    from aux
-  ;;
-
-  let uncombine xs =
-    let whosfirst = ref `left
-    and lq = Queue.create ()
-    and rq = Queue.create () in
-    let rec left i =
-      match !whosfirst with
-      | `left -> (
-        match next xs with
-        | None -> None
-        | Some (l, r) ->
-          Queue.enqueue rq r;
-          Some l)
-      | `right -> (
-        match Queue.dequeue lq with
-        | None ->
-          whosfirst := `left;
-          left i
-        | x -> x)
-    and right i =
-      match !whosfirst with
-      | `right -> (
-        match next xs with
-        | None -> None
-        | Some (l, r) ->
-          Queue.enqueue lq l;
-          Some r)
-      | `left -> (
-        match Queue.dequeue rq with
-        | None ->
-          whosfirst := `right;
-          right i
-        | x -> x)
-    in
-    from left, from right
-  ;;
-
-  let merge xs ys ~cmp =
-    let aux _ =
-      match peek xs, peek ys with
-      | (Some x as ex), Some y when cmp x y <= 0 ->
-        junk xs;
-        ex
-      | Some _, (Some _ as ey) ->
-        junk ys;
-        ey
-      | (Some _ as ex), None ->
-        junk xs;
-        ex
-      | None, (Some _ as ey) ->
-        junk ys;
-        ey
-      | None, None -> None
-    in
-    from aux
-  ;;
-
-  let partition xs ~f =
-    let pos_queue = Queue.create ()
-    and neg_queue = Queue.create () in
-    let rec pos i =
-      match Queue.dequeue pos_queue with
-      | None -> (
-        match next xs with
-        | Some x when not (f x) ->
-          Queue.enqueue neg_queue x;
-          pos i
-        | e -> e)
-      | e -> e
-    and neg i =
-      match Queue.dequeue neg_queue with
-      | None -> (
-        match next xs with
-        | Some x when f x ->
-          Queue.enqueue pos_queue x;
-          neg i
-        | e -> e)
-      | e -> e
-    in
-    from pos, from neg
-  ;;
-
-  let uniq xs =
-    match peek xs with
-    | None -> empty ()
-    | Some first ->
-      let prev = ref first in
-      let rec aux i =
-        if i = 0
-        then Some first
-        else (
-          match next xs with
-          | None -> None
-          | Some x ->
-            if Poly.(x = !prev)
-            then aux i
-            else (
-              prev := x;
-              Some x))
-      in
-      from aux
-  ;;
-
   let init n ~f =
     if n < 0
     then empty ()
@@ -599,28 +235,7 @@ module Stream = struct
       | len -> Some (Bytes.To_string.sub buf ~pos:0 ~len))
   ;;
 
-  let of_array a =
-    Stream.from (fun i ->
-      try Some a.(i) with
-      | Invalid_argument _ -> None)
-  ;;
-
-  let to_array strm = Array.of_list (to_list strm)
   let of_hashtbl t = of_list (Hashtbl.to_alist t)
-
-  let to_hashtbl xs =
-    let t = Hashtbl.Poly.create () in
-    iter xs ~f:(fun (key, data) -> Hashtbl.Poly.set t ~key ~data);
-    t
-  ;;
-
-  let of_map t = of_list (Map.to_alist t)
-
-  let to_map xs =
-    fold xs ~init:Map.Poly.empty ~f:(fun accu (key, data) -> Map.Poly.set accu ~key ~data)
-  ;;
-
-  let of_set t = of_list (Set.to_list t)
   let to_set xs = fold xs ~init:Set.Poly.empty ~f:(fun accu e -> Set.Poly.add accu e)
 
   module Result = struct
--- lib/unix/CFStream.mli
+++ lib/unix/CFStream.mli
@@ -94,9 +94,6 @@ module Stream : sig
   (** The empty stream. *)
   val empty : unit -> 'a t
 
-  (** [init n f] returns the stream [f 0; f 1; ... f (n-1)]. *)
-  val init : int -> f:(int -> 'a) -> 'a t
-
   (** [singleton x] returns a stream containing the single value [x]. *)
   val singleton : 'a -> 'a t
 
@@ -111,9 +108,6 @@ module Stream : sig
     The stream is infinite if [f] never returns None. *)
   val unfold : 'a -> f:('a -> ('b * 'a) option) -> 'b t
 
-  (** Indexed variant of [unfold] *)
-  val unfoldi : 'a -> f:(int -> 'a -> ('b * 'a) option) -> 'b t
-
   val of_lazy : 'a t lazy_t -> 'a t
 
   (** {6 Iterators}
@@ -125,16 +119,6 @@ module Stream : sig
 
   (** [iter xs ~f] calls in turn [f x0], [f x1], ... *)
   val iter : 'a t -> f:('a -> unit) -> unit
-
-  (** Like [iter] but operates on two streams. Stops when either stream
-    becomes empty. *)
-  val iter2 : 'a t -> 'b t -> f:('a -> 'b -> unit) -> unit
-
-  (** Like [iter2] except streams required to be of equal length.
-    @raise Expected_streams_of_equal_length if the two streams have
-    different lengths, in which case there is no guarantee about which
-    elements were consumed. *)
-  val iter2_exn : 'a t -> 'b t -> f:('a -> 'b -> unit) -> unit
 
   (** [fold xs ~init ~f] returns [f (...(f (f init x0) x1)...) xn], that
     is for the stream [a0; a1; ...; an] does the following calculations:
@@ -148,98 +132,6 @@ module Stream : sig
 *)
   val fold : 'a t -> init:'b -> f:('b -> 'a -> 'b) -> 'b
 
-  (** Like [fold] but operates on two streams. Processing continues
-    until either stream becomes empty. *)
-  val fold2 : 'a t -> 'b t -> init:'c -> f:('c -> 'a -> 'b -> 'c) -> 'c
-
-  (** Like [fold2] except streams required to be of equal length.
-    @raise Expected_streams_of_equal_length if the two streams have
-    different lengths, in which case there is no guarantee about which
-    elements were consumed. *)
-  val fold2_exn : 'a t -> 'b t -> init:'c -> f:('c -> 'a -> 'b -> 'c) -> 'c
-
-  (** Like [fold] but all intermediate values are returned, not just the
-    final value. If given stream [s] is [a0; a1; ...], then [scanl f
-    init s] is the stream containing
-
-    - b0 = init
-    - b1 = f b0 a0
-    - b2 = f b1 a1
-    - ...
-. *)
-  val scanl : 'a t -> init:'b -> f:('b -> 'a -> 'b) -> 'b t
-
-  (** [scan] is similar to [scanl] but without the [init] value: if [s]
-    contains [x0], [x1], [x2] ..., [scan s ~f] contains
-
-    - y0 = x0
-    - y1 = f y0 x1
-    - y2 = f y1 x2
-    - ...
-
-    For instance, [scan (1 -- 10) ~f:( * )] will produce an
-    enumeration containing the successive values of the factorial
-    function. Returns an empty stream if the input stream is empty as
-    well. *)
-  val scan : 'a t -> f:('a -> 'a -> 'a) -> 'a t
-
-  (** Indexed variants of the previous higher-order functions. The index
-    provided to the [~f] argument is the count of the stream, that is
-    the number of discarded elements before the reaching the current
-    one. For functions iterating on two streams, the [~f] is thus
-    provided two indices, since the current count may differ from one
-    stream to another. *)
-  val iteri : 'a t -> f:(int -> 'a -> unit) -> unit
-
-  val iter2i_exn : 'a t -> 'b t -> f:(int -> int -> 'a -> 'b -> unit) -> unit
-  val iter2i : 'a t -> 'b t -> f:(int -> int -> 'a -> 'b -> unit) -> unit
-  val foldi : 'a t -> init:'b -> f:(int -> 'b -> 'a -> 'b) -> 'b
-  val fold2i_exn : 'a t -> 'b t -> init:'c -> f:(int -> int -> 'c -> 'a -> 'b -> 'c) -> 'c
-  val fold2i : 'a t -> 'b t -> init:'c -> f:(int -> int -> 'c -> 'a -> 'b -> 'c) -> 'c
-
-  (** [reduce xs ~f] returns [f (...(f (f x1 x2) x3)...) xn] *)
-  val reduce : 'a t -> f:('a -> 'a -> 'a) -> 'a
-
-  (** [sum xs] returns the sum of the integers contained in [xs] *)
-  val sum : int t -> int
-
-  (** [fsum xs] returns the sum of the floats contained in [xs] *)
-  val fsum : float t -> float
-
-  (** {6 Scanners}
-    Operations that scan a stream for various purposes. Unlike
-    iterators, these operations are not inherently meant to consume
-    streams, although they do partially or fully, due to the nature of
-    streams.
-*)
-
-  (** [exists s ~f] returns [true] if there is some [x] in [s] such that
-    [f x] is true. The stream is consumed through and including
-    [x]. *)
-  val exists : 'a t -> f:('a -> bool) -> bool
-
-  (** [for_all s ~f] returns [true] if [f x] is true for every [x] in
-    [s]. *)
-  val for_all : 'a t -> f:('a -> bool) -> bool
-
-  (** [find e ~f] returns either [Some x] where [x] is the first
-    element of [e] such that [f x] returns [true], consuming the
-    stream up to and including the found element, or [None] if no
-    such element exists in the stream, consuming the whole stream in
-    the search.
-
-    Since [find] (eagerly) consumes a prefix of the stream, it
-    can be used several times on the same stream to find the
-    next element. *)
-  val find : 'a t -> f:('a -> bool) -> 'a option
-
-  (** Same as [find] except that it raises an exception [Not_found]
-    instead of returning [None]. *)
-  val find_exn : 'a t -> f:('a -> bool) -> 'a
-
-  (** Similar to [find] *)
-  val find_map : 'a t -> f:('a -> 'b option) -> 'b option
-
   (** {6 Converters}
     Extract a subset of a stream or map a stream into another type of
     stream.
@@ -248,17 +140,15 @@ module Stream : sig
   (** [take xs ~n] builds a fresh stream from [xs] containing the [d]
     first elements of [xs] where [d = min n l] and [l] is the length
     of [xs]. As it is fresh, the count of the resulting stream starts
-    from [0] whatever the count of [xs] is. *)
-  val take : 'a t -> n:int -> 'a t
+    from [0] whatever the count of [xs] is.
 
-  (** Same as [take] but takes elements from the input enum as long as
+    Same as [take] but takes elements from the input enum as long as
     [f] evaluates to [true]. *)
   val take_while : 'a t -> f:('a -> bool) -> 'a t
 
-  (** [drop xs ~n] is equivalent to calling [n] times [junk] on [xs]. *)
-  val drop : 'a t -> n:int -> unit
+  (** [drop xs ~n] is equivalent to calling [n] times [junk] on [xs].
 
-  (** Similar to [drop]: [drop_while xs ~f] removes elements from [xs]
+  Similar to [drop]: [drop_while xs ~f] removes elements from [xs]
     and stops when [f] evals to false on the head element. *)
   val drop_while : 'a t -> f:('a -> bool) -> unit
 
@@ -266,79 +156,11 @@ module Stream : sig
     chained composition). *)
   val skip : 'a t -> n:int -> 'a t
 
-  (** Similar to [skip]: [skip_while xs ~f] removes elements from [xs]
-    and stops when [f] evals to false on the head element. *)
-  val skip_while : 'a t -> f:('a -> bool) -> 'a t
-
-  (** Indexed variants of the previous prefix/suffix constructors *)
-  val take_whilei : 'a t -> f:(int -> 'a -> bool) -> 'a t
-
-  val drop_whilei : 'a t -> f:(int -> 'a -> bool) -> unit
-  val skip_whilei : 'a t -> f:(int -> 'a -> bool) -> 'a t
-
-  (** [span test e] produces two streams [(hd, tl)], such that
-    [hd] is the same as [take_while test e] and [tl] is the same
-    as [skip_while test e]. *)
-  val span : 'a t -> f:('a -> bool) -> 'a t * 'a t
-
-  (** [group xs f] applies [f] to the elements of [xs] and distribute
-    them according to the return value of [f]. Let [ys] = [group xs
-    f], then [xs] = [concat ys] and in each stream [s] of [ys], all
-    values give the same value with [f]. *)
-  val group : 'a t -> f:('a -> 'b) -> 'a t t
-
-  (** Same as [group] but with a comparison function instead of a
-    mapping. *)
-  val group_by : 'a t -> eq:('a -> 'a -> bool) -> 'a t t
-
-  (** Given a stream with items [x0, x1, x2, x3,...], the returned stream
-    will be pairs of items [(x0,x1), (x2,x3), ...].
-
-    @raise Premature_end_of_input if input stream has an odd number of
-    elements. *)
-  val chunk2 : 'a t -> ('a * 'a) t
-
-  (** Like [chunk2] but for 3-tuples. *)
-  val chunk3 : 'a t -> ('a * 'a * 'a) t
-
-  (** Like [chunk2] but for 4-tuples. *)
-  val chunk4 : 'a t -> ('a * 'a * 'a * 'a) t
-
   val map : 'a t -> f:('a -> 'b) -> 'b t
-  val mapi : 'a t -> f:(int -> 'a -> 'b) -> 'b t
-  val map2_exn : 'a t -> 'b t -> f:('a -> 'b -> 'c) -> 'c t
-  val mapi2_exn : 'a t -> 'b t -> f:(int -> 'a -> 'b -> 'c) -> 'c t
   val filter : 'a t -> f:('a -> bool) -> 'a t
-  val filter_map : 'a t -> f:('a -> 'b option) -> 'b t
   val append : 'a t -> 'a t -> 'a t
   val concat : 'a t t -> 'a t
   val concat_map : 'a t -> f:('a -> 'b t) -> 'b t
-
-  (** [combine] transforms a pair of streams into a stream of pairs of
-    corresponding elements. If one stream is short, excess elements
-    of the longer stream are ignored. *)
-  val combine : 'a t * 'b t -> ('a * 'b) t
-
-  (** [uncombine] is the opposite of [combine] *)
-  val uncombine : ('a * 'b) t -> 'a t * 'b t
-
-  (** [merge test (a, b)] merge the elements from [a] and [b] into a
-      single stream. At each step, [test] is applied to the first
-      element of [a] and the first element of [b] to determine which
-      should get first into the resulting stream. If [a] or [b]
-      runs out of elements, the process will append all elements of
-      the other stream to the result.  *)
-  val merge : 'a t -> 'a t -> cmp:('a -> 'a -> int) -> 'a t
-
-  (** [partition e ~f] splits [e] into two streams, where the first
-      stream have all the elements satisfying [f], the second stream
-      is opposite. The order of elements in the source stream is
-      preserved. *)
-  val partition : 'a t -> f:('a -> bool) -> 'a t * 'a t
-
-  (** [uniq e] returns a duplicate of [e] with repeated values
-      omitted. (similar to unix's [uniq] command) *)
-  val uniq : 'a t -> 'a t
 
   (** {6 Data Interchange}
     Convert/create a stream to/from another data structure.
@@ -346,13 +168,7 @@ module Stream : sig
 
   val of_list : 'a list -> 'a t
   val to_list : 'a t -> 'a list
-  val of_array : 'a array -> 'a t
-  val to_array : 'a t -> 'a array
   val of_hashtbl : ('a, 'b) Hashtbl.t -> ('a * 'b) t
-  val to_hashtbl : ('a * 'b) t -> ('a, 'b) Hashtbl.t
-  val of_map : ('a, 'b, 'c) Map.t -> ('a * 'b) t
-  val to_map : ('a * 'b) t -> ('a, 'b) Map.Poly.t
-  val of_set : ('a, 'b) Set.t -> 'a t
   val to_set : 'a t -> 'a Set.Poly.t
   val of_string : string -> char t
 
