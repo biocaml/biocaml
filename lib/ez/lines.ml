9bbb9528557b16ed4563df9cfdac72eab7484279
Ashish Agarwal <ashish@solvuu.com>
2025-07-02 16:24:39 -0500

    Avoid open Biocaml_unix.CFStream

--- lib/ez/lines.ml
+++ lib/ez/lines.ml
@@ -1,8 +1,9 @@
-open Biocaml_unix.CFStream
 include Biocaml_unix.Lines
 
 let file_mapper inbed outbed ~f =
   In_channel.with_file inbed ~f:(fun ic ->
     Out_channel.with_file outbed ~f:(fun oc ->
-      read ic |> Stream.concat_map ~f:(fun x -> Stream.of_list (f x)) |> write oc))
+      read ic
+      |> Biocaml_unix.CFStream.Stream.concat_map ~f:(fun x -> Stream.of_list (f x))
+      |> write oc))
 ;;
