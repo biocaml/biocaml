073332bb4900c15c9774605e4192803e782e28da
Ashish Agarwal <ashish@solvuu.com>
2025-09-16 15:06:19 -0400

    Add Sam tests

--- lib/base/sam.ml
+++ lib/base/sam.ml
@@ -1465,186 +1465,329 @@ let of_string s = s |> String.split_lines |> of_lines
 let of_string_exn s = s |> String.split_lines |> of_lines_exn
 
 module Test = struct
-  (* module Sam = Biocaml_unix.Sam_deprecated *)
-  (* module Tfxm = Biocaml_unix.Tfxm *)
+  open Expect_test_helpers_base
 
-  (* let test_parser_deprecated () = *)
-  (*   let transfo = Sam.Transform.string_to_raw () in *)
-  (*   let test_line l f = *)
-  (*     Tfxm.feed transfo (l ^ "\n"); *)
-  (*     assert_bool l (f (Tfxm.next transfo)) *)
-  (*   in *)
-  (*   let test_output l o = test_line l (fun oo -> `output (Ok o) = oo) in *)
+  let%expect_test "Header.Item.of_string" =
+    let data =
+      [ "@CO\tsome comment"
+      ; "@HD\tVN:1.3\tSO:coordinate"
+      ; "@SQ\tSN:chr0\tLN:42"
+      ; "@SQ\tSN:chr1\tLN:42\tM5:abd34f90"
+      ; "@HD\tT"
+      ]
+    in
+    let test x =
+      let result = Header.Item.of_string x in
+      print_string
+        (sprintf
+           "IN: \"%s\"\nSEXP:\n%s\n"
+           x
+           ([%sexp_of: Header.Item.t Or_error.t] result |> sexp_to_string))
+    in
+    List.iter data ~f:test;
+    [%expect
+      {|
+      IN: "@CO	some comment"
+      SEXP:
+      (Ok (CO "some comment"))
 
-  (*   test_output "@CO\tsome comment" (`comment "some comment"); *)
-  (*   test_output "@HD\tVN:1.3\tSO:coordinate" *)
-  (*     (`header ("HD", [ "VN", "1.3"; "SO", "coordinate"])); *)
-  (*   test_output "r001\t83\tref\t37\t30\t9M\t=\t7\t-39\tCAGCGCCAT\t*" *)
-  (*     (`alignment *)
-  (*         {Sam.qname = "r001"; flag = 83; rname = "ref"; pos = 37; mapq = 30; *)
-  (*          cigar = "9M"; rnext = "="; pnext = 7; tlen = -39; seq = "CAGCGCCAT"; *)
-  (*          qual = "*"; optional = []}); *)
-  (*   test_output "r001\t83\tref\t37\t30\t9M\t=\t7\t-39\tCAGCGCCAT\t*\tNM:i:0" *)
-  (*     (`alignment *)
-  (*         {Sam.qname = "r001"; flag = 83; rname = "ref"; pos = 37; mapq = 30; *)
-  (*          cigar = "9M"; rnext = "="; pnext = 7; tlen = -39; seq = "CAGCGCCAT"; *)
-  (*          qual = "*"; optional = [("NM", 'i', "0")]}); *)
-  (*   test_output "r001\t83\tref\t37\t30\t9M\t=\t7\t-39\tCAGCGCCAT\t*\tNM:i:0\tKJ:A:a" *)
-  (*     (`alignment *)
-  (*         {Sam.qname = "r001"; flag = 83; rname = "ref"; pos = 37; mapq = 30; *)
-  (*          cigar = "9M"; rnext = "="; pnext = 7; tlen = -39; seq = "CAGCGCCAT"; *)
-  (*          qual = "*"; optional = [("NM", 'i', "0"); ("KJ", 'A', "a")]}); *)
-  (*   test_line "r001\t83\tref\t37\t30\t9M\t=\t7\t-39\tCAGCGCCAT\t*\tNM:i:0\tKJ:A" *)
-  (*     (function *)
-  (*     | `output (Error (`wrong_optional_field (_, _))) -> true *)
-  (*     | _ -> false); *)
-  (*   test_line "r001\t83\tref\t37\t30\t9M\t=\t7h\t-39\tCAGCGCCAT\t*\tNM:i:0\tKJ:A:a" *)
-  (*     (function *)
-  (*     | `output (Error (`not_an_int (_, "pnext", "7h")))  -> true *)
-  (*     | _ -> false); *)
-  (*   test_line  "r001\t83\tref\t37\t30\t9M\t=\t7\t-39\tCAGCGCCAT" *)
-  (*     (function *)
-  (*     | `output (Error (`wrong_alignment (_, _))) -> true *)
-  (*     | _ -> false); *)
-  (*   test_line "@HD\tT" *)
-  (*     (function *)
-  (*     | `output (Error (`invalid_tag_value_list (_, ["T"]))) -> true *)
-  (*     | _ -> false); *)
-  (*   () *)
+      IN: "@HD	VN:1.3	SO:coordinate"
+      SEXP:
+      (Ok (HD ((version 1.3) (sort_order (Coordinate)) (group_order ()))))
 
-  (* let test_item_parser_deprecated () = *)
-  (*   let c = ref 0 in *)
-  (*   let check t v f = *)
-  (*     let p = Sam.Transform.raw_to_string () in *)
-  (*     incr c; *)
-  (*     Tfxm.feed t v; *)
-  (*     let res = f (Tfxm.next t) in *)
-  (*     if not res then *)
-  (*       eprintf "Error on %s\n" *)
-  (*         Tfxm.( *)
-  (*           match feed p v; next p with `output o -> o | _ -> failwith "printer!" *)
-  (*         ); *)
-  (*     assert_bool (sprintf "test_item_parser.check %d" !c) res *)
-  (*   in *)
-  (*   let check_output t v o = check t v ((=) (`output (Ok o))) in *)
+      IN: "@SQ	SN:chr0	LN:42"
+      SEXP:
+      (Ok (
+        SQ (
+          (name   chr0)
+          (length 42)
+          (assembly ())
+          (md5      ())
+          (species  ())
+          (uri      ()))))
 
-  (*   let t = Sam.Transform.raw_to_item () in *)
-  (*   check_output t (`comment "comment") (`comment "comment"); *)
-  (*   check t (`header ("HD", ["VN", "42.1"])) (function *)
-  (*   | `output (Error (`header_line_not_first 2)) -> true *)
-  (*   | _ -> false); *)
+      IN: "@SQ	SN:chr1	LN:42	M5:abd34f90"
+      SEXP:
+      (Ok (
+        SQ (
+          (name   chr1)
+          (length 42)
+          (assembly ())
+          (md5 (abd34f90))
+          (species ())
+          (uri     ()))))
 
-  (*   let t = Sam.Transform.raw_to_item () in *)
-  (*   check_output t (`header ("HD", ["VN", "42.1"])) *)
-  (*     (`header_line ("42.1", `unknown, [])); *)
-  (*   check t (`header ("SQ", [])) (function *)
-  (*   | `output (Error (`missing_ref_sequence_name [])) -> true *)
-  (*   | _ -> false); *)
-  (*   check t (`header ("SQ", ["SN", "chr0"])) (function *)
-  (*   | `output (Error (`missing_ref_sequence_length [("SN", "chr0")])) -> true *)
-  (*   | _ -> false); *)
-  (*   check t (`header ("SQ", ["SN", "chr0"; "LN", "not an int"])) (function *)
-  (*   | `output (Error (`wrong_ref_sequence_length _))  -> true *)
-  (*   | _ -> false); *)
+      IN: "@HD	T"
+      SEXP:
+      (Error ("tag-value not colon separated" T))
+      |}]
+  ;;
 
-  (*   let t = Sam.Transform.raw_to_item () in *)
-  (*   check_output t (`header ("HD", ["VN", "42.1"; *)
-  (*                                   "SO", "coordinate"; "HP", "some other"])) *)
-  (*     (`header_line ("42.1", `coordinate, [("HP", "some other")])); *)
-  (*   check t (`header ("SQ", ["SN", "chr0"; "LN", "42"])) ((=) `not_ready); *)
-  (*   check t (`header ("SQ", ["SN", "chr1"; "LN", "42"; "M5", "abd34f90"])) *)
-  (*     ((=) `not_ready); *)
-  (*   (\* the ref-info is being buffered, the first alignment will output it *\) *)
-  (*   check_output t (`alignment *)
-  (*             {Sam.qname = "r001"; flag = 83; rname = "ref"; pos = 37; mapq = 30; *)
-  (*              cigar = "9M"; rnext = "="; pnext = 7; tlen = -39; seq = "CAGCGCCAT"; *)
-  (*              qual = "*"; optional = [("NM", 'i', "0")]}) *)
-  (*     (`reference_sequence_dictionary *)
-  (*         [| *)
-  (*           {Sam.ref_name = "chr0"; ref_length = 42; ref_assembly_identifier = None; *)
-  (*            ref_checksum = None; ref_species = None; ref_uri = None; *)
-  (*            ref_unknown = []}; *)
-  (*           {Sam.ref_name = "chr1"; ref_length = 42; ref_assembly_identifier = None; *)
-  (*            ref_checksum = Some "abd34f90"; ref_species = None; ref_uri = None; *)
-  (*            ref_unknown = []}; *)
-  (*         |]); *)
-  (*   (\* This one get the previous alignment: *\) *)
-  (*   check_output t *)
-  (*     (`alignment *)
-  (*         {Sam.qname = "chr0"; flag = 83; rname = "chr0"; pos = 37; mapq = 30; *)
-  (*          cigar = "9M"; rnext = "*"; pnext = 7; tlen = -39; seq = "CAGCGCCAT"; *)
-  (*          qual = "*"; optional = [("NM", 'i', "0")]}) *)
-  (*     (`alignment *)
-  (*         {Sam.query_template_name = "r001"; flag = Sam.Flag.of_int 83; *)
-  (*          reference_sequence = `name "ref"; position = Some 37; *)
-  (*          mapping_quality = Some 30; cigar_operations = [|`M 9|]; *)
-  (*          next_reference_sequence = `qname; next_position = Some 7; *)
-  (*          template_length = Some (-39); sequence = `string "CAGCGCCAT"; *)
-  (*          quality = [| |]; optional_content = [ "NM", 'i', `int 0] }); *)
-  (*   Tfxm.stop t; *)
-  (*   (\* We still have one to get: *\) *)
-  (*   assert_bool "last alignment" (Tfxm.next t = *)
-  (*       `output (Ok *)
-  (*                  (`alignment *)
-  (*                      {Sam.query_template_name = "chr0"; flag = Sam.Flag.of_int 83; *)
-  (*                       reference_sequence = *)
-  (*                          `reference_sequence *)
-  (*                            {Sam.ref_name = "chr0"; ref_length = 42; *)
-  (*                             ref_assembly_identifier = None; *)
-  (*                             ref_checksum = None; ref_species = None; ref_uri = None; *)
-  (*                             ref_unknown = []}; *)
-  (*                       position = Some 37; mapping_quality = Some 30; *)
-  (*                       cigar_operations = [|`M 9|]; next_reference_sequence = `none; *)
-  (*                       next_position = Some 7; template_length = Some (-39); *)
-  (*                       sequence = `string "CAGCGCCAT"; quality = [| |]; *)
-  (*                       optional_content = [ "NM", 'i', `int 0]}))); *)
-  (*   assert_bool "EOS" (Tfxm.next t = `end_of_stream); *)
+  let%expect_test "Alignment.of_string" =
+    let data =
+      [ "r001\t83\tref\t37\t30\t9M\t=\t7\t-39\tCAGCGCCAT\t*"
+      ; "r001\t83\tref\t37\t30\t9M\t=\t7\t-39\tCAGCGCCAT\t*\tNM:i:0"
+      ; "r001\t83\tref\t37\t30\t9M\t=\t7\t-39\tCAGCGCCAT\t*\tNM:i:0\tKJ:A:a"
+      ; "r001\t83\tref\t37\t30\t9M\t=\t7\t-39\tCAGCGCCAT\t*\tNM:i:0\tKJ:A"
+      ; "r001\t83\tref\t37\t30\t9M\t=\t7h\t-39\tCAGCGCCAT\t*\tNM:i:0\tKJ:A:a"
+      ; "r001\t83\tref\t37\t30\t9M\t=\t7\t-39\tCAGCGCCAT"
+      ; "r001\t83\tref\t37\t30\t9M\t=\t7\t-39\tCAGCGCCAT\t!''*((((("
+      ]
+    in
+    let test x =
+      let result = Alignment.of_string x in
+      print_string
+        (sprintf
+           "IN: \"%s\"\nSEXP:\n%s\n"
+           x
+           ([%sexp_of: Alignment.t Or_error.t] result |> sexp_to_string))
+    in
+    List.iter data ~f:test;
+    [%expect
+      {|
+      IN: "r001	83	ref	37	30	9M	=	7	-39	CAGCGCCAT	*"
+      SEXP:
+      (Ok (
+        (qname (r001))
+        (flag 83)
+        (rname (ref))
+        (pos   (37))
+        (mapq  (30))
+        (cigar ((Alignment_match 9)))
+        (rnext (Equal_to_RNAME))
+        (pnext (7))
+        (tlen  (-39))
+        (seq   (CAGCGCCAT))
+        (qual            ())
+        (optional_fields ())))
 
-  (*
-     check (`header ("HD", ["VN", "42.1"; "SO", "coordinate"]));
-  check (`header ("HD", ["VN", "42.1"; "SO", "wut?"]));
-  check (`header ("HD", ["VN", "42.1"; "SO", "coordinate"; "HP", "some other"]));
-  check (`header ("HD", []));
-  check (`header ("SQ", []));
-  check (`header ("SQ", ["SN", "chr0"]));
-  check (`header ("SQ", ["SN", "chr0"; "LN", "not an int"]));
-  check (`header ("SQ", ["SN", "chr0"; "LN", "42"]));
-  check (`header ("SQ", ["SN", "chr1"; "LN", "42"; "M5", "abd34f90"]));
-  check (`header ("RR", ["SN", "chr1"; "LN", "42"; "M5", "abd34f90"]));
-  check (`alignment
-            {qname = "r001"; flag = 83; rname = "ref"; pos = 37; mapq = 30;
-             cigar = "9M"; rnext = "="; pnext = 7; tlen = -39; seq = "CAGCGCCAT";
-             qual = "*"; optional = [("NM", 'i', "0")]});
+      IN: "r001	83	ref	37	30	9M	=	7	-39	CAGCGCCAT	*	NM:i:0"
+      SEXP:
+      (Ok (
+        (qname (r001))
+        (flag 83)
+        (rname (ref))
+        (pos   (37))
+        (mapq  (30))
+        (cigar ((Alignment_match 9)))
+        (rnext (Equal_to_RNAME))
+        (pnext (7))
+        (tlen  (-39))
+        (seq   (CAGCGCCAT))
+        (qual ())
+        (optional_fields (((tag NM) (value (i 0)))))))
 
-  check (`alignment
-            {qname = "chr0"; flag = 83; rname = "chr0"; pos = 37; mapq = 30;
-             cigar = "9M"; rnext = "*"; pnext = 7; tlen = -39; seq = "CAGCGCCAT";
-             qual = "*"; optional = [("NM", 'i', "0")]});
-  *)
+      IN: "r001	83	ref	37	30	9M	=	7	-39	CAGCGCCAT	*	NM:i:0	KJ:A:a"
+      SEXP:
+      (Ok (
+        (qname (r001))
+        (flag 83)
+        (rname (ref))
+        (pos   (37))
+        (mapq  (30))
+        (cigar ((Alignment_match 9)))
+        (rnext (Equal_to_RNAME))
+        (pnext (7))
+        (tlen  (-39))
+        (seq   (CAGCGCCAT))
+        (qual ())
+        (optional_fields (
+          ((tag NM) (value (i 0)))
+          ((tag KJ) (value (A a)))))))
 
-  (* let test_printer_deprecated () = *)
-  (*   let transfo = Sam.Transform.raw_to_string () in *)
-  (*   let test_line i l = *)
-  (*     Tfxm.feed transfo i; *)
-  (*     assert_bool l (Tfxm.next transfo = `output (l ^ "\n")) *)
-  (*   in *)
+      IN: "r001	83	ref	37	30	9M	=	7	-39	CAGCGCCAT	*	NM:i:0	KJ:A"
+      SEXP:
+      (Error ("missing TYPE in optional field" A))
 
-  (*   test_line *)
-  (*     (`alignment *)
-  (*         {Sam.qname = "r001"; flag = 83; rname = "ref"; pos = 37; mapq = 30; *)
-  (*          cigar = "9M"; rnext = "="; pnext = 7; tlen = -39; seq = "CAGCGCCAT"; *)
-  (*          qual = "*"; optional = [("NM", 'i', "0")]}) *)
-  (*     "r001\t83\tref\t37\t30\t9M\t=\t7\t-39\tCAGCGCCAT\t*\tNM:i:0"; *)
+      IN: "r001	83	ref	37	30	9M	=	7h	-39	CAGCGCCAT	*	NM:i:0	KJ:A:a"
+      SEXP:
+      (Error ("PNEXT not an int" 7h))
 
-  (*   test_line (`comment "some comment") "@CO\tsome comment" ; *)
-  (*   test_line (`header ("HD", [ "VN", "1.3"; "SO", "coordinate"])) *)
-  (*     "@HD\tVN:1.3\tSO:coordinate"; *)
-  (*   () *)
+      IN: "r001	83	ref	37	30	9M	=	7	-39	CAGCGCCAT"
+      SEXP:
+      (Error "alignment line contains < 12 fields")
 
-  (* let tests = "SAM" >::: [ *)
-  (*     "Parse SAM" >:: test_parser ; *)
-  (*     "Parse SAM raw" >:: test_parser_deprecated; *)
-  (*     "Print SAM" >:: test_printer_deprecated; *)
-  (*     "Parse SAM item" >:: test_item_parser_deprecated; *)
-  (*   ] *)
+      IN: "r001	83	ref	37	30	9M	=	7	-39	CAGCGCCAT	!''*((((("
+      SEXP:
+      (Ok (
+        (qname (r001))
+        (flag 83)
+        (rname (ref))
+        (pos   (37))
+        (mapq  (30))
+        (cigar ((Alignment_match 9)))
+        (rnext (Equal_to_RNAME))
+        (pnext (7))
+        (tlen  (-39))
+        (seq   (CAGCGCCAT))
+        (qual (0 6 6 9 7 7 7 7 7))
+        (optional_fields ())))
+      |}]
+  ;;
+
+  let%expect_test "Cigar.of_string" =
+    let data = [ "9M"; "8M2I4M1D3M" ] in
+    let test x =
+      let result = Cigar.of_string x in
+      print_string
+        (sprintf
+           "IN: \"%s\"\nSEXP:\n%s\n"
+           x
+           ([%sexp_of: Cigar.t Or_error.t] result |> sexp_to_string))
+    in
+    List.iter data ~f:test;
+    [%expect
+      {|
+      IN: "9M"
+      SEXP:
+      (Ok ((Alignment_match 9)))
+
+      IN: "8M2I4M1D3M"
+      SEXP:
+      (Ok (
+        (Alignment_match 8)
+        (Insertion       2)
+        (Alignment_match 4)
+        (Deletion        1)
+        (Alignment_match 3)))
+      |}]
+  ;;
+
+  let%expect_test "Flag.of_string" =
+    let data = [ "83" ] in
+    let test x =
+      let result = Flag.of_string x in
+      print_string
+        (sprintf
+           "IN: \"%s\"\nSEXP: %s\n"
+           x
+           ([%sexp_of: Flag.t Or_error.t] result |> sexp_to_string))
+    in
+    List.iter data ~f:test;
+    [%expect
+      {|
+      IN: "83"
+      SEXP: (Ok 83)
+      |}]
+  ;;
+
+  let%expect_test "Optional_field.of_string" =
+    let data = [ "NM:i:0"; "KJ:A:a"; "RG:Z:sample1"; "AS:f:123.45" ] in
+    let test x =
+      let result = Optional_field.of_string x in
+      print_string
+        (sprintf
+           "IN: \"%s\"\nSEXP: %s\n"
+           x
+           ([%sexp_of: Optional_field.t Or_error.t] result |> sexp_to_string))
+    in
+    List.iter data ~f:test;
+    [%expect
+      {|
+      IN: "NM:i:0"
+      SEXP: (Ok ((tag NM) (value (i 0))))
+
+      IN: "KJ:A:a"
+      SEXP: (Ok ((tag KJ) (value (A a))))
+
+      IN: "RG:Z:sample1"
+      SEXP: (Ok ((tag RG) (value (Z sample1))))
+
+      IN: "AS:f:123.45"
+      SEXP: (Ok ((tag AS) (value (f 123.45))))
+      |}]
+  ;;
+
+  let%expect_test "of_lines" =
+    let data =
+      [ [ "@HD\tVN:1.3\tSO:coordinate"
+        ; "@SQ\tSN:chr1\tLN:249250621"
+        ; "@CO\tThis is a comment"
+        ; "r001\t83\tref\t37\t30\t9M\t=\t7\t-39\tCAGCGCCAT\t*\tNM:i:0"
+        ; "r002\t0\tchr1\t100\t30\t10M\t*\t0\t0\tACGTACGTAC\t!''*(((((\tRG:Z:sample1"
+        ]
+      ; [ "@HD\tVN:1.3\tSO:coordinate"
+        ; "@SQ\tSN:chr1\tLN:249250621"
+        ; "@CO\tThis is a comment"
+        ; "r001\t83\tref\t37\t30\t9M\t=\t7\t-39\tCAGCGCCAT\t*\tNM:i:0"
+        ; "r002\t0\tchr1\t100\t30\t10M\t*\t0\t0\tACGTACGTAC\t*\tRG:Z:sample1"
+        ]
+      ; [ "@SQ\tSN:chr1\tLN:249250621"
+        ; "@HD\tVN:1.3\tSO:coordinate"
+        ; "r002\t0\tchr1\t100\t30\t10M\t*\t0\t0\tACGTACGTAC\t*\tRG:Z:sample1"
+        ]
+      ]
+    in
+    let test lines =
+      let result = of_lines lines in
+      print_string "IN:\n";
+      List.iter lines ~f:(fun x -> print_string (sprintf "%s\n" x));
+      print_string
+        (sprintf
+           "SEXP:\n%s\n"
+           ([%sexp_of: (Header.t * Alignment.t list) Or_error.t] result |> sexp_to_string))
+    in
+    List.iter data ~f:test;
+    [%expect
+      {|
+      IN:
+      @HD	VN:1.3	SO:coordinate
+      @SQ	SN:chr1	LN:249250621
+      @CO	This is a comment
+      r001	83	ref	37	30	9M	=	7	-39	CAGCGCCAT	*	NM:i:0
+      r002	0	chr1	100	30	10M	*	0	0	ACGTACGTAC	!''*(((((	RG:Z:sample1
+      SEXP:
+      (Error ("SEQ and QUAL lengths differ" (10 9)))
+
+      IN:
+      @HD	VN:1.3	SO:coordinate
+      @SQ	SN:chr1	LN:249250621
+      @CO	This is a comment
+      r001	83	ref	37	30	9M	=	7	-39	CAGCGCCAT	*	NM:i:0
+      r002	0	chr1	100	30	10M	*	0	0	ACGTACGTAC	*	RG:Z:sample1
+      SEXP:
+      (Ok (
+        ((HD ((version 1.3) (sort_order (Coordinate)) (group_order ())))
+         (SQ (
+           (name   chr1)
+           (length 249250621)
+           (assembly ())
+           (md5      ())
+           (species  ())
+           (uri      ())))
+         (CO "This is a comment"))
+        (((qname (r001))
+          (flag 83)
+          (rname (ref))
+          (pos   (37))
+          (mapq  (30))
+          (cigar ((Alignment_match 9)))
+          (rnext (Equal_to_RNAME))
+          (pnext (7))
+          (tlen  (-39))
+          (seq   (CAGCGCCAT))
+          (qual ())
+          (optional_fields (((tag NM) (value (i 0))))))
+         ((qname (r002))
+          (flag 0)
+          (rname (chr1))
+          (pos   (100))
+          (mapq  (30))
+          (cigar ((Alignment_match 10)))
+          (rnext ())
+          (pnext ())
+          (tlen  ())
+          (seq (ACGTACGTAC))
+          (qual ())
+          (optional_fields (((tag RG) (value (Z sample1)))))))))
+
+      FIXME: The error message is incorrect.
+      IN:
+      @SQ	SN:chr1	LN:249250621
+      @HD	VN:1.3	SO:coordinate
+      r002	0	chr1	100	30	10M	*	0	0	ACGTACGTAC	*	RG:Z:sample1
+      SEXP:
+      (Error "multiple @HD lines not allowed")
+      |}]
+  ;;
 end
